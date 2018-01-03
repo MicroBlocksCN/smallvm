22fa72302fa3ae05e60d2173ac0352714204f200
Bernat Romagosa <bromagosa@e-citilab.eu>
2018-01-03 11:44:42 +0100

    [connector] autoinstall node modules if missing

--- connector/node/start.sh
+++ connector/node/start.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+if ! test -e node_modules; then
+    echo "Missing some node.js modules. I'll try to install them for you now."
+    if ! test -n `which npm`; then
+        echo "Missing node.js package manager (npm). Please check https://www.npmjs.com/get-npm for instructions on how to install it for your system."
+        exit 1
+    else
+        npm install
+    fi
+fi
+
 node index.js $*
