1790fcaf41289333f0f681dce612df84331452ac
John Maloney <jhmaloney@gmail.com>
2018-02-04 20:53:36 -0500

    Update linux.c to read/write on a pseudo-terminal. The IDE can connect to the associated tty and interact with the MicroBlocks VM.

--- linux/linux.c
+++ linux/linux.c
@@ -5,9 +5,12 @@
 // John Maloney, December 2017
 // Bernat Romagosa, February 2018
 
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
 #include <unistd.h>
-#include <time.h>
 
 #include "mem.h"
 #include "interp.h"
@@ -16,145 +19,93 @@
 // Timing Functions
 
 uint32 microsecs() {
-    return millisecs() * 100; // just testing, give me a break! :)
+	struct timeval now;
+	gettimeofday(&now, NULL);
+
+    return (uint32) ((1000000 * now.tv_sec) + now.tv_usec);
 }
+
 uint32 millisecs() {
-    return (uint32) 5;
+	struct timeval now;
+	gettimeofday(&now, NULL);
+
+    return (uint32) ((1000 * now.tv_sec) + (now.tv_usec / 1000));
 }
 
 // Communication/System Functions
 
-static int serialPort;
+static int pty; // pseudo terminal used for communication with the IDE
 
-void putSerial(char *s) { puts(s); }
+static void openPseudoTerminal() {
+	pty = posix_openpt(O_RDWR | O_NOCTTY | O_NONBLOCK);
+	if (-1 == pty) {
+		perror("Error opening pseudo terminal\n");
+		exit(-1);
+	}
+ 	grantpt(pty);
+ 	unlockpt(pty);
+}
 
 int readBytes(uint8 *buf, int count) {
-	int readCount = 0;
-        char currentChar;
-	while (readCount < count) {
-            currentChar = getchar();
-            if (currentChar ) return readCount;
-            buf[readCount++] = currentChar;
-	}
+	int readCount = read(pty, buf, count);
+	if (readCount < 0) readCount = 0;
 	return readCount;
 }
 
-int canReadByte() { return true; }
+int canReadByte() {
+	int bytesAvailable;
+	ioctl(pty, FIONREAD, &bytesAvailable);
+	return (bytesAvailable > 0);
+}
+
 int canSendByte() { return true; }
-void sendByte(char aByte) { printf("%c", aByte); }
+
+void sendByte(char aByte) {
+//printf("byte %d %c\n", aByte, ((aByte > 0) ? aByte : ' '));
+	write(pty, &aByte, 1);
+}
 
 // System Functions
 
 const char * boardType() { return "Linux Computer"; }
 void systemReset() { } // noop on Linux
 
-// General Purpose I/O Pins
+// Stubs for IO primitives
 
-#define DIGITAL_PINS 0
-#define ANALOG_PINS 0
-#define TOTAL_PINS (DIGITAL_PINS + ANALOG_PINS)
-#define PIN_LED 0
-
-// Pin Modes
-
-// To speed up pin I/O, the current pin input/output mode is recorded in the currentMode[]
-// array to avoid calling pinMode() unless the pin mode has actually changed.
-
-static char currentMode[TOTAL_PINS];
-
-#define MODE_NOT_SET (-1)
-
-#define SET_MODE(pin, newMode) { \
-	if ((newMode) != currentMode[pin]) { \
-		pinMode((pin), newMode); \
-		currentMode[pin] = newMode; \
-	} \
-}
-
-static void initPins(void) {
-	// Initialize currentMode to MODE_NOT_SET (neigher INPUT nor OUTPUT)
-	// to force the pin's mode to be set on first use.
-
-	for (int i; i < TOTAL_PINS; i++) currentMode[i] = MODE_NOT_SET;
-}
-
-// Pin IO Primitives
-
-OBJ primAnalogPins(OBJ *args) { return int2obj(ANALOG_PINS); }
-OBJ primDigitalPins(OBJ *args) { return int2obj(DIGITAL_PINS); }
-
-OBJ primAnalogRead(OBJ *args) { return int2obj(0); } // no analog inputs
-OBJ primAnalogWrite(OBJ *args) { return nilObj; } // analog output not supported
-
-OBJ primDigitalRead(OBJ *args) {
-	return nilObj;
-}
-
-OBJ primDigitalWrite(OBJ *args) {
-	return nilObj;
-}
-
-OBJ primSetLED(OBJ *args) {
-	return nilObj;
-}
-
-OBJ primI2cGet(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primI2cSet(OBJ *args) {
-	return nilObj;
-}
+OBJ primAnalogPins(OBJ *args) { return int2obj(0); }
+OBJ primDigitalPins(OBJ *args) { return int2obj(0); }
+OBJ primAnalogRead(OBJ *args) { return int2obj(0); }
+OBJ primAnalogWrite(OBJ *args) { return nilObj; }
+OBJ primDigitalRead(OBJ *args) { return nilObj; }
+OBJ primDigitalWrite(OBJ *args) { return nilObj; }
+OBJ primSetLED(OBJ *args) { return nilObj; }
+OBJ primI2cGet(OBJ *args) { return nilObj; }
+OBJ primI2cSet(OBJ *args) { return nilObj; }
+OBJ primSPISend(OBJ *args) { return nilObj; }
+OBJ primSPIRecv(OBJ *args) { return nilObj; }
 
 // Bogus micro:bit primitives
-OBJ primMBDisplay(OBJ *args) {
-        return nilObj;
-}
 
-OBJ primMBDisplayOff(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBPlot(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBUnplot(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBTiltX(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBTiltY(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBTiltZ(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBTemp(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBButtonA(OBJ *args) {
-        return nilObj;
-}
-
-OBJ primMBButtonB(OBJ *args) {
-        return nilObj;
-}
-
+OBJ primMBDisplay(OBJ *args) { return nilObj; }
+OBJ primMBDisplayOff(OBJ *args) { return nilObj; }
+OBJ primMBPlot(OBJ *args) { return nilObj; }
+OBJ primMBUnplot(OBJ *args) { return nilObj; }
+OBJ primMBTiltX(OBJ *args) { return nilObj; }
+OBJ primMBTiltY(OBJ *args) { return nilObj; }
+OBJ primMBTiltZ(OBJ *args) { return nilObj; }
+OBJ primMBTemp(OBJ *args) { return nilObj; }
+OBJ primMBButtonA(OBJ *args) { return nilObj; }
+OBJ primMBButtonB(OBJ *args) { return nilObj; }
 
 // Linux Main
 
 int main() {
-    memInit(5000); // 5k words = 20k bytes
-    restoreScripts();
-    startAll();
-    printf("MicroBlocks is running...\n");
+	openPseudoTerminal();
+    printf("Starting Linux MicroBlocks... Connect on %s\n", ptsname(pty));
+    memInit(10000); // 10k words = 40k bytes
+    initTasks();
+	restoreScripts();
     outputString("Welcome to uBlocks for Linux!");
     vmLoop();
+    return 0;
 }
