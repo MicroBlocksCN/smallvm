d4947c1b4a9e4e5af2be803ac22b9eb75152c809
John Maloney <jhmaloney@gmail.com>
2018-04-24 13:31:56 -0400

    Add license notices to vm files

--- vm/interp.c
+++ vm/interp.c
@@ -1,3 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Copyright 2018 John Maloney, Bernat Romagosa, and Jens Mönig
+
 // interp.c - Simple interpreter based on 32-bit opcodes
 // John Maloney, April 2017
 
--- vm/vm.ino
+++ vm/vm.ino
@@ -1,3 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Copyright 2018 John Maloney, Bernat Romagosa, and Jens Mönig
+
 #include "mem.h"
 #include "interp.h"
 #include "persist.h"
@@ -16,4 +22,3 @@ void setup() {
 void loop() {
   vmLoop();
 }
-
