e52541e36f4ebd099f846dbbb415d42163b19fe7
John Maloney <jhmaloney@gmail.com>
2018-04-24 13:54:20 -0400

    Add license notices

--- linux/linux.c
+++ linux/linux.c
@@ -1,3 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Copyright 2018 John Maloney, Bernat Romagosa, and Jens Mönig
+
 // linux.c - Microblocks for Linux
 // An adaptation of raspberryPi.c by John Maloney to
 // work on a GNU/Linux TTY as a serial device
