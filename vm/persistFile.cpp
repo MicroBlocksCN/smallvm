0b57878544098fd2cddbc368200d50e16019452a
John Maloney <jhmaloney@gmail.com>
2018-05-10 08:42:03 +0200

    Clean up persistFile.cpp Use "extern C" on individual functions Tweak indentation

--- vm/persistFile.cpp
+++ vm/persistFile.cpp
@@ -5,7 +5,7 @@
 // Copyright 2018 John Maloney, Bernat Romagosa, and Jens Mönig
 
 // persistFile.cpp - Persistent file memory for code and variables
-// Bernat Romagosa
+// Bernat Romagosa and John Maloney
 
 #include <stdio.h>
 #include <FS.h>
@@ -16,61 +16,29 @@
 
 File codeFile;
 
-extern "C" {
-    void initCodeFile(uint8 *flash, int flashByteCount) {
-        char s[100];
-       SPIFFS.begin();
+extern "C" void initCodeFile(uint8 *flash, int flashByteCount) {
+	SPIFFS.begin();
+	codeFile = SPIFFS.open("ublockscode", "a+");
 
-sprintf(s, "initCodeFile flash address is %d, flash bytes: %d", flash, flashByteCount);
-outputString(s);
+	// read code file into simulated Flash:
+	long int bytesRead = codeFile.readBytes((char*) flash, flashByteCount);
 
-//SPIFFS.format(); // xxx
-        codeFile = SPIFFS.open("ublockscode", "a+");
-
-int fileSize = codeFile.size();
-sprintf(s, "file size: %d, position: %d", fileSize, codeFile.position());
-outputString(s);
-
-		codeFile.seek(0, SeekSet);
-
-sprintf(s, "position after seek: %d", codeFile.position());
-outputString(s);
-
-        // read code file straight into memory
-//int bytesRead = 0;
-        long int bytesRead = codeFile.readBytes((char*) flash, flashByteCount);
-
-        sprintf(s, "read %d bytes into memory (file size %d)\n", bytesRead, fileSize);
-        outputString(s);
-    }
-
-
-    void writeCodeFile(uint8 *code, int byteCount) {
 char s[100];
-outputString("writeCodeFile...");
-int oldSize = codeFile.size();
-
-		int bytesWritten = codeFile.write(code, byteCount);
-		codeFile.flush();
-
-sprintf(s, "  %d bytes of %d appended to code file; size: %d -> %d\n",
-	bytesWritten, byteCount, oldSize, codeFile.size());
+sprintf(s, "Read %d bytes from code file (file size %d)", bytesRead, codeFile.size());
 outputString(s);
-    }
+}
 
-    void clearCodeFile() {
-        char s[100];
-outputString("clearing code file");
-        codeFile.close();
-        SPIFFS.remove("ublockscode");
-        codeFile = SPIFFS.open("ublockscode", "a+");
-        uint32 cycleCount = ('S' << 24) | 1;
-        int bytesWritten = codeFile.write((uint8 *) &cycleCount, 4);
-		codeFile.flush();
+extern "C" void writeCodeFile(uint8 *code, int byteCount) {
+	int bytesWritten = codeFile.write(code, byteCount);
+	codeFile.flush();
+}
 
-sprintf(s, "   wrote %d header bytes", bytesWritten);
-outputString(s);
-        clearPersistentMemory();
-outputString("clearCodeFile done");
-   }
+extern "C"  void clearCodeFile() {
+	codeFile.close();
+	SPIFFS.remove("ublockscode");
+	codeFile = SPIFFS.open("ublockscode", "a+");
+	uint32 cycleCount = ('S' << 24) | 1; // Header record, version 1
+	int bytesWritten = codeFile.write((uint8 *) &cycleCount, 4);
+	codeFile.flush();
+outputString("Code file cleared");
 }
