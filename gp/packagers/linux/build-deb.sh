e5ba07e89f48cb6274d6ba04324ff6ae4cd46fe0
Bernat Romagosa <bromagosa@e-citilab.eu>
2018-06-28 12:39:18 +0200

    added menu file for alternative Linux WMs

--- gp/packagers/linux/build-deb.sh
+++ gp/packagers/linux/build-deb.sh
@@ -22,6 +22,7 @@ if [ -z "$arch" ]; then arch="all"; fi
 mkdir -p deb/ublocks/usr/local/bin
 mkdir -p deb/ublocks/usr/share/icons
 mkdir -p deb/ublocks/usr/share/applications
+mkdir -p deb/ublocks/usr/share/menu
 mkdir -p deb/ublocks/DEBIAN
 mkdir -p deb/ublocks/usr/share/doc/ublocks
 chmod 0755 deb/ublocks/usr -R
@@ -30,9 +31,11 @@ cp $exepath deb/ublocks/usr/local/bin/ublocks
 cp MicroBlocks.png deb/ublocks/usr/share/icons
 cp MicroBlocks.desktop deb/ublocks/usr/share/applications
 cp copyright deb/ublocks/usr/share/doc/ublocks
+cp microBlocks.menu deb/ublocks/usr/share/menu
 
 size=`du deb/ublocks | tail -n1 | cut -f1`
 cat control | sed -E "s/@AppVersion/$version/" | sed -E "s/@Arch/$arch/" | sed -E "s/@InstalledSize/$size/"> deb/ublocks/DEBIAN/control
+cp prerm postinst deb/ublocks/DEBIAN/
 
 cd deb
 fakeroot dpkg-deb --build ublocks
