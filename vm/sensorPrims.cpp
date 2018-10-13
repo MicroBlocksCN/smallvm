287a803e80d45a61294b52dd0208bf652d71d8c9
John Maloney <jhmaloney@gmail.com>
2018-10-13 10:46:19 -0400

    Add support for new micro:bit accelerometer chip

--- vm/sensorPrims.cpp
+++ vm/sensorPrims.cpp
@@ -32,13 +32,7 @@ OBJ primI2cGet(OBJ *args) {
 	if (!wireStarted) startWire();
 	Wire.beginTransmission(deviceID);
 	Wire.write(registerID);
-	// xxx Should this releasing the i2c bus (on ESP32) when we're about to request data?
-	// Need to revisit how i2c works...
-#ifdef ARDUINO_ARCH_ESP32
-	int error = Wire.endTransmission();
-#else
-	int error = Wire.endTransmission(false);
-#endif
+	int error = Wire.endTransmission((bool) false);
 	if (error) return int2obj(0 - error); // error; bad device ID?
 
 	Wire.requestFrom(deviceID, 1);
@@ -88,11 +82,82 @@ OBJ primSPIRecv(OBJ *args) {
 
 #if defined(ARDUINO_BBC_MICROBIT) || defined(ARDUINO_SINOBIT)
 
-#define ACCEL_ID 29
+typedef enum {
+	accel_unknown = -1,
+	accel_none = 0,
+	accel_MMA8653 = 1,
+	accel_LSM303 = 2,
+	accel_FXOS8700 = 3,
+} AccelerometerType_t;
 
-static int accelerometerOn = false;
+static AccelerometerType_t accelType = accel_unknown;
+
+#define ACCEL_ID 29 // xxx
+#define MMA8653_ID 29
+#define LSM303_ID 25
+#define FXOS8700_ID 30
+
+static int accelerometerOn = false; // xxx
+
+static int readI2CReg(int deviceID, int reg) {
+	if (!wireStarted) startWire();
+	Wire.beginTransmission(deviceID);
+	Wire.write(reg);
+	int error = Wire.endTransmission(false);
+	if (error) return -error; // error; bad device ID?
+
+	Wire.requestFrom(deviceID, 1);
+	while (!Wire.available());
+	return Wire.read();
+}
+
+static void writeI2CReg(int deviceID, int reg, int value) {
+	if (!wireStarted) startWire();
+	Wire.beginTransmission(deviceID);
+	Wire.write(reg);
+	Wire.write(value);
+	Wire.endTransmission();
+}
+
+static int detectAccelerometer() {
+	if (0x5A == readI2CReg(MMA8653_ID, 0x0D)) {
+		accelType = accel_MMA8653;
+		writeI2CReg(MMA8653_ID, 0x2A, 1);
+	} else if (0x33 == readI2CReg(LSM303_ID, 0x0F)) {
+		accelType = accel_LSM303;
+		writeI2CReg(LSM303_ID, 0x20, 0x5F); // 100 Hz sample rate, low power, all axes
+	} else if (0xC7 == readI2CReg(FXOS8700_ID, 0x0D)) {
+		accelType = accel_FXOS8700;
+		// xxx initialize FXOS8700
+	} else {
+		accelType = accel_none;
+	}
+}
 
 static int readAcceleration(int registerID) {
+	if (accel_unknown == accelType) detectAccelerometer();
+	int sign = -1;
+	int val = 0;
+	switch (accelType) {
+	case accel_MMA8653:
+		val = readI2CReg(MMA8653_ID, registerID);
+		break;
+	case accel_LSM303:
+		if (1 == registerID) { val = readI2CReg(LSM303_ID, 0x29); sign = 1; } // x-axis
+		if (3 == registerID) val = readI2CReg(LSM303_ID, 0x2B); // y-axis
+		if (5 == registerID) val = readI2CReg(LSM303_ID, 0x2D); // z-axis
+		break;
+	case accel_FXOS8700:
+		// xxx read FXOS8700 data
+		break;
+	}
+	val = (val >= 128) ? (val - 256) : val; // value is a signed byte
+	if (val < -127) val = -127; // keep in range -127 to 127
+	val = sign * ((val * 100) / 127); // scale to range 0-100 and multiply by sign
+	return val;
+}
+
+static int OLDreadAcceleration(int registerID) {
 	if (!accelerometerOn) {
 		// turn on the accelerometer
 		if (!wireStarted) startWire();
