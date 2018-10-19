7924e21fa3b1c722747f7b71526a564a51f2af39
John Maloney <jhmaloney@gmail.com>
2018-10-19 09:59:24 -0400

    Tweak because board defintion for Calliope changed name to CalliopeMini

--- updatePrecompiled.sh
+++ updatePrecompiled.sh
@@ -3,7 +3,7 @@
 # You must first use the Arduino IDE to export the compiled binary for each board.
 
 cp vm/vm.ino.BBCmicrobit.hex precompiled
-cp vm/vm.ino.Calliope.hex precompiled
+cp vm/vm.ino.CalliopeMini.hex precompiled/vm.ino.Calliope.hex
 cp vm/vm.ino.nodemcu.bin precompiled
 python uf2conv.py -c vm/vm.ino.circuitplay.bin -o precompiled/vm.circuitplay.uf2
 python uf2conv.py -c vm/vm.ino.itsybitsy_m0.bin -o precompiled/vm.itsybitsy.uf2
