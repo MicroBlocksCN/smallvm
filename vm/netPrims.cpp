9717fa5929726d6901b2533363ddd62237e23e0c
John Maloney <jhmaloney@gmail.com>
2018-10-30 10:47:43 -0400

    Reformat to use tabs rather than spaces; no other changes

--- vm/netPrims.cpp
+++ vm/netPrims.cpp
@@ -13,10 +13,10 @@
 #include "tinyJSON.h"
 
 #if defined(ESP8266)
-  #include <ESP8266WiFi.h>
+	#include <ESP8266WiFi.h>
 #elif defined(ARDUINO_ARCH_ESP32)
-  #include <WiFi.h>
-  #include <esp_wifi.h>
+	#include <WiFi.h>
+	#include <esp_wifi.h>
 #endif
 
 #include "interp.h" // must be included *after* ESP8266WiFi.h
@@ -57,18 +57,18 @@ WiFiServer server(80);
 WiFiClient client;
 
 void primWifiConnect(OBJ *args) {
-  // don't cancel ongoing connection attempts
-  if (!connecting) {
-    connecting = true;
-    initTime = millisecs();
-    char *essid = obj2str(args[0]);
-    char *psk = obj2str(args[1]);
-    // Kill active connection, if there was one
-    WiFi.persistent(false);
-    WiFi.mode(WIFI_OFF);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(essid, psk);
-  }
+	// don't cancel ongoing connection attempts
+	if (!connecting) {
+		connecting = true;
+		initTime = millisecs();
+		char *essid = obj2str(args[0]);
+		char *psk = obj2str(args[1]);
+		// Kill active connection, if there was one
+		WiFi.persistent(false);
+		WiFi.mode(WIFI_OFF);
+		WiFi.mode(WIFI_STA);
+		WiFi.begin(essid, psk);
+	}
 }
 
 // Web Server for Mozilla IoT Things
@@ -85,7 +85,6 @@ static int hasPrefix(char *s, char *requestPrefix, char *arg, int argSize) {
 	// the next space will be copied into arg.
 
 	if (arg) arg[0] = '\0'; // clear arg result, if provided
-
 	if ((strstr(s, requestPrefix) != s)) return false; // request does not match prefix
 
 	if (arg) {
@@ -97,127 +96,130 @@ static int hasPrefix(char *s, char *requestPrefix, char *arg, int argSize) {
 		int count = end - start;
 		if (count > argSize) count = argSize;
 		if (count > 0) {
-		  strncpy(arg, start, count);
-		  arg[count] = '\0'; // null terminate
+			strncpy(arg, start, count);
+			arg[count] = '\0'; // null terminate
 		}
 	}
 	return true;
 }
 
 static char * valueJSON(char *response, char *varName, int varID) {
-  // Write a object with the name and value of the given variable into response.
+	// Write a object with the name and value of the given variable into response.
 
-  OBJ value = vars[varID];
-  if (isInt(value)) {
-	sprintf(response, "{ \"%s\": %d }\r\n", varName, obj2int(value));
-  } else if ((trueObj == value) || (falseObj == value)) {
-	sprintf(response, "{ \"%s\": %s }\r\n", varName, (trueObj == value) ? "true" : "false");
-  } else if (IS_CLASS(value, StringClass)) {
-	sprintf(response, "{ \"%s\": \"%s\" }\r\n", varName, obj2str(value));
-  } else {
-    sprintf(response, "{ \"%s\": \"<Unknown type>\" }\r\n", varName);
-  }
-  return response;
+	OBJ value = vars[varID];
+	if (isInt(value)) {
+		sprintf(response, "{ \"%s\": %d }\r\n", varName, obj2int(value));
+	} else if ((trueObj == value) || (falseObj == value)) {
+		sprintf(response, "{ \"%s\": %s }\r\n", varName, (trueObj == value) ? "true" : "false");
+	} else if (IS_CLASS(value, StringClass)) {
+		sprintf(response, "{ \"%s\": \"%s\" }\r\n", varName, obj2str(value));
+	} else {
+		sprintf(response, "{ \"%s\": \"<Unknown type>\" }\r\n", varName);
+	}
+	return response;
 }
 
 static void setVariableValue(char *varName, int varID, char *jsonData) {
-  // Set the value of the given variable from the given JSON data.
+	// Set the value of the given variable from the given JSON data.
 
-  char *p = tjr_atPath(jsonData, varName);
-  int type = tjr_type(p);
-  if (tjr_Number == type) {
-    vars[varID] = int2obj(tjr_readInteger(p));
-  } else if (tjr_String == type) {
-    char s[100];
-    tjr_readStringInto(p, s, sizeof(s));
-    vars[varID] = newStringFromBytes((uint8 *) s, strlen(s));
-  } else if (tjr_True == type) {
-    vars[varID] = trueObj;
-  } else if (tjr_False == type) {
-    vars[varID] = falseObj;
-  }
+	char *p = tjr_atPath(jsonData, varName);
+	int type = tjr_type(p);
+	if (tjr_Number == type) {
+		vars[varID] = int2obj(tjr_readInteger(p));
+	} else if (tjr_String == type) {
+		char s[100];
+		tjr_readStringInto(p, s, sizeof(s));
+		vars[varID] = newStringFromBytes((uint8 *) s, strlen(s));
+	} else if (tjr_True == type) {
+		vars[varID] = trueObj;
+	} else if (tjr_False == type) {
+		vars[varID] = falseObj;
+	}
 }
 
 void webServerLoop() {
-  if (!client) client = server.available(); // attempt to accept a client connection
-  if (!client) return; // no client connection
+	if (!client) client = server.available(); // attempt to accept a client connection
+	if (!client) return; // no client connection
 
-  // read an HTTP request
-  int bytesAvailable = client.available();
-  if (!bytesAvailable) return;
-  client.readBytes(request, bytesAvailable);
-  request[bytesAvailable] = 0; // null terminate
+	// read an HTTP request
+	int bytesAvailable = client.available();
+	if (!bytesAvailable) return;
+	client.readBytes(request, bytesAvailable);
+	request[bytesAvailable] = 0; // null terminate
 
-  // the body starts after the first blank line (or at end of request, if no blank lines)
-  char *body = strstr(request, "\r\n\r\n");
-  body = body ? (body + 4) : request + strlen(request);
+	// the body starts after the first blank line (or at end of request, if no blank lines)
+	char *body = strstr(request, "\r\n\r\n");
+	body = body ? (body + 4) : request + strlen(request);
 
-  char response[1000];
-  char varName[100];
-  int varID = -1;
+	char response[1000];
+	char varName[100];
+	int varID = -1;
 
-  // HTTP request format: "[GET/PUT] /some/url HTTP/1.1"
-  if (hasPrefix(request, "GET / ", NULL, 0)) {
-    // Get the Thing description
-    client.print(JSON_HEADER);
-    client.print(descriptionObj.body);
-  } else if (hasPrefix(request, "GET /properties/", varName, sizeof(varName))) {
-    // Get variable value
-    varID = indexOfVarNamed(varName);
-    if (varID < 0) {
-      client.print(NOT_FOUND_RESPONSE);
-    } else {
-	  client.print(JSON_HEADER);
-      client.print(valueJSON(response, varName, varID));
-    }
-  } else if (hasPrefix(request, "PUT /properties/", varName, sizeof(varName))) {
-    // Set variable value
-    varID = indexOfVarNamed(varName);
-    if (varID < 0) {
-      client.print(NOT_FOUND_RESPONSE);
-    } else {
-      setVariableValue(varName, varID, body);
-	  client.print(JSON_HEADER);
-      client.print(valueJSON(response, varName, varID));
-    }
-  } else {
-    client.print(NOT_FOUND_RESPONSE);
-  }
-  client.flush();
-  client.stop();
+	// HTTP request format: "[GET/PUT] /some/url HTTP/1.1"
+	if (hasPrefix(request, "GET / ", NULL, 0)) {
+		// Get the Thing description
+		client.print(JSON_HEADER);
+		client.print(descriptionObj.body);
+	} else if (hasPrefix(request, "GET /properties/", varName, sizeof(varName))) {
+		// Get variable value
+		varID = indexOfVarNamed(varName);
+		if (varID < 0) {
+			client.print(NOT_FOUND_RESPONSE);
+		} else {
+			client.print(JSON_HEADER);
+			client.print(valueJSON(response, varName, varID));
+		}
+	} else if (hasPrefix(request, "PUT /properties/", varName, sizeof(varName))) {
+		// Set variable value
+		varID = indexOfVarNamed(varName);
+		if (varID < 0) {
+			client.print(NOT_FOUND_RESPONSE);
+		} else {
+			setVariableValue(varName, varID, body);
+			client.print(JSON_HEADER);
+			client.print(valueJSON(response, varName, varID));
+		}
+	} else {
+		client.print(NOT_FOUND_RESPONSE);
+	}
+	client.flush();
+	client.stop();
 }
 
 // WiFi Connections (old)
 
 int wifiStatus() {
-  //  WL_IDLE_STATUS      = 0
-  //  WL_CONNECTED        = 3
-  //  WL_CONNECT_FAILED   = 4
-  //  WL_CONNECTION_LOST  = 5
-  //  WL_DISCONNECTED     = 6
-  int status = WiFi.status();
-  if (status == 3 && WiFi.localIP()[0] != 0 && millisecs() > initTime + 250) {
-    // Got an IP. We're online. We wait at least a quarter second, otherwise
-    // we may have read an old state
-    connecting = false;
-    initWebServer();
-  } else if (status != 3 && millisecs() > initTime + 10000) {
-    // We time out after 10s
-    WiFi.disconnect();
-    status = WL_DISCONNECTED;
-    connecting = false;
-    fail(couldNotJoinWifiNetwork);
-  } else {
-    // Still waiting
-    status = WL_IDLE_STATUS;
-  }
-  return status;
+	// WL_IDLE_STATUS		= 0
+	// WL_CONNECTED			= 3
+	// WL_CONNECT_FAILED	= 4
+	// WL_CONNECTION_LOST	= 5
+	// WL_DISCONNECTED		= 6
+	int status = WiFi.status();
+	if (status == 3 && WiFi.localIP()[0] != 0 && millisecs() > initTime + 250) {
+		// Got an IP. We're online. We wait at least a quarter second, otherwise
+		// we may have read an old state
+		connecting = false;
+		initWebServer();
+	} else if (status != 3 && millisecs() > initTime + 10000) {
+		// We time out after 10s
+		WiFi.disconnect();
+		status = WL_DISCONNECTED;
+		connecting = false;
+		fail(couldNotJoinWifiNetwork);
+	} else {
+		// Still waiting
+		status = WL_IDLE_STATUS;
+	}
+	return status;
 }
 
 // WiFi Connections (new)
 
+// Macro for creating MicroBlocks string object constants
 #define STRING_OBJ_CONST(s) \
-  struct { uint32 header = HEADER(StringClass, ((sizeof(s) + 3) / 4)); char body[sizeof(s)] = s; }
+	struct { uint32 header = HEADER(StringClass, ((sizeof(s) + 3) / 4)); char body[sizeof(s)] = s; }
+
+// Status strings that can be returned by WiFiStatus primitive
 
 STRING_OBJ_CONST("Not connected") statusNotConnected;
 STRING_OBJ_CONST("Trying...") statusTrying;
@@ -228,38 +230,36 @@ STRING_OBJ_CONST("Unknown network") statusUnknownNetwork;
 int firstTime = true;
 
 OBJ primStartWiFi(int argCount, OBJ *args) {
-  // Start a WiFi connection attempt. The client should call wifiStatus until either
-  // the connection is established or the attempt fails.
+	// Start a WiFi connection attempt. The client should call wifiStatus until either
+	// the connection is established or the attempt fails.
 
-  if (argCount < 2) return fail(notEnoughArguments);
+	if (argCount < 2) return fail(notEnoughArguments);
 
-  char *networkName = obj2str(args[0]);
-  char *password = obj2str(args[1]);
-  int createHotSpot = (argCount > 2) && (trueObj == args[2]);
+	char *networkName = obj2str(args[0]);
+	char *password = obj2str(args[1]);
+	int createHotSpot = (argCount > 2) && (trueObj == args[2]);
 
-  WiFi.persistent(false); // don't save network info to Flash
-  WiFi.mode(WIFI_OFF); // Kill the current connection, if any
+	WiFi.persistent(false); // don't save network info to Flash
+	WiFi.mode(WIFI_OFF); // Kill the current connection, if any
 
-  if (createHotSpot) {
-    WiFi.mode(WIFI_AP_STA); // access point & station mode
-    WiFi.softAP(networkName, password);
-  } else {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(networkName, password);
-  }
+	if (createHotSpot) {
+		WiFi.mode(WIFI_AP_STA); // access point & station mode
+		WiFi.softAP(networkName, password);
+	} else {
+		WiFi.mode(WIFI_STA);
+		WiFi.begin(networkName, password);
+	}
 
-#ifdef ESP8266
-  // workaround for an apparent ESP8266 WiFi startup bug; calling WiFi.status() during
-  // the first few seconds after starting WiFi for the first time results in strange
-  // behavior (task just stops without either error or completion; memory corruption?)
-  if (firstTime) {
-    delay(3000); // 3000 works, 2500 does not
-    firstTime = false;
-  }
-#endif
-
-outputString("VM: Connecting...");
-  connecting = true;
+	#ifdef ESP8266
+		// workaround for an apparent ESP8266 WiFi startup bug: calling WiFi.status() during
+		// the first few seconds after starting WiFi for the first time results in strange
+		// behavior (task just stops without either error or completion; memory corruption?)
+		if (firstTime) {
+			delay(3000); // 3000 works, 2500 does not
+			firstTime = false;
+		}
+	#endif
+	connecting = true;
 }
 
 OBJ primStopWiFi(int argCount, OBJ *args) {
@@ -306,48 +306,48 @@ struct {
 } ipStringObject;
 
 OBJ primGetIP(int argCount, OBJ *args) {
-  IPAddress ip = (WIFI_AP_STA == WiFi.getMode()) ? WiFi.softAPIP() : WiFi.localIP();
-  sprintf(ipStringObject.body, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
-  ipStringObject.header = HEADER(StringClass, (strlen(ipStringObject.body) + 4) / 4);
-  return (OBJ) &ipStringObject;
+	IPAddress ip = (WIFI_AP_STA == WiFi.getMode()) ? WiFi.softAPIP() : WiFi.localIP();
+	sprintf(ipStringObject.body, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+	ipStringObject.header = HEADER(StringClass, (strlen(ipStringObject.body) + 4) / 4);
+	return (OBJ) &ipStringObject;
 }
 
 // Thing Description (old)
 
 OBJ primMakeWebThing(int argCount, OBJ *args) {
-  char* thingName = obj2str(args[0]);
-  int bytesWritten = sprintf(
-    descriptionObj.body,
-    "{\"name\":\"%s\","
-    "\"@type\":\"MicroBlocks\","
-    "\"description\":\"%s\","
-    "\"href\":\"/\","
-    "\"properties\":{",
-    thingName,
-    thingName
-  );
-  for (int i = 1; i < argCount; i += 3) {
-    char* propertyType = obj2str(args[i]);
-    char* propertyLabel = obj2str(args[i+1]);
-    char* propertyVar = obj2str(args[i+2]);
-    bytesWritten += sprintf(
-      descriptionObj.body + bytesWritten,
-      "\"%s\":"
-        "{\"type\":\"%s\","
-         "\"label\":\"%s\","
-         "\"href\":\"/properties/%s\""
-        "},",
-      propertyVar,
-      propertyType,
-      propertyLabel,
-      propertyVar
-    );
-  }
-  if (argCount > 2) {
-    // we subtract one position to overwrite the last comma
-    bytesWritten --;
-  }
-  sprintf(descriptionObj.body + bytesWritten, "}}\0");
+	char* thingName = obj2str(args[0]);
+	int bytesWritten = sprintf(
+		descriptionObj.body,
+			"{\"name\":\"%s\","
+			"\"@type\":\"MicroBlocks\","
+			"\"description\":\"%s\","
+			"\"href\":\"/\","
+			"\"properties\":{",
+		thingName,
+		thingName
+	);
+	for (int i = 1; i < argCount; i += 3) {
+		char* propertyType = obj2str(args[i]);
+		char* propertyLabel = obj2str(args[i+1]);
+		char* propertyVar = obj2str(args[i+2]);
+		bytesWritten += sprintf(
+			descriptionObj.body + bytesWritten,
+				"\"%s\":"
+				"{\"type\":\"%s\","
+				"\"label\":\"%s\","
+				"\"href\":\"/properties/%s\""
+				"},",
+			propertyVar,
+			propertyType,
+			propertyLabel,
+			propertyVar
+		);
+	}
+	if (argCount > 2) {
+		// we subtract one position to overwrite the last comma
+		bytesWritten --;
+	}
+	sprintf(descriptionObj.body + bytesWritten, "}}\0");
 }
 
 // Thing Description (new)
