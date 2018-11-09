eeac36471bf9f25687d5b2c2685549d1c445b4a4
John Maloney <jhmaloney@gmail.com>
2018-11-09 12:31:34 -0500

    VM v046 Compiled Binaries Add precompiled VM for ESP32 (specifically, the OddWires IoT-Bus, bus should work with most ESP32 boards (although user LED may be on a different pin)

--- updatePrecompiled.sh
+++ updatePrecompiled.sh
@@ -5,5 +5,6 @@
 cp vm/vm.ino.BBCmicrobit.hex precompiled
 cp vm/vm.ino.CalliopeMini.hex precompiled/vm.ino.Calliope.hex
 cp vm/vm.ino.nodemcu.bin precompiled
+cp vm/vm.ino.lolin32.bin precompiled/vm.ino.esp32.bin
 python uf2conv.py -c vm/vm.ino.circuitplay.bin -o precompiled/vm.circuitplay.uf2
 python uf2conv.py -c vm/vm.ino.itsybitsy_m0.bin -o precompiled/vm.itsybitsy.uf2
