718d6d3db902c0f71f2ba3bcc52c52cbef0ada86
John Maloney <jhmaloney@gmail.com>
2019-10-21 19:04:39 -0400

    Added Circuit Playground Bluefruit nrf52 to precompiled build script

--- precompiled/updatePrecompiled.sh
+++ precompiled/updatePrecompiled.sh
@@ -17,6 +17,8 @@ pio run -e esp32
 cp .pio/build/esp32/firmware.bin precompiled/vm.ino.esp32.bin
 pio run -e cpx
 python precompiled/uf2conv.py -c .pio/build/cpx/firmware.bin -o precompiled/vm.circuitplay.uf2
+pio run -e cplay52
+python precompiled/uf2conv.py -c .pio/build/cplay52/firmware.hex -f 0xADA52840 -o precompiled/vm.cplay52.uf2
 pio run -e itsybitsy
 python precompiled/uf2conv.py -c .pio/build/itsybitsy/firmware.bin -o precompiled/vm.itsybitsy.uf2
 cd precompiled
