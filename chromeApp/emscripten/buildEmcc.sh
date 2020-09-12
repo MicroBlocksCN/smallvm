b7ada6232417a9dfb40f7f5c74e89b52ca3b36d4
John Maloney <jhmaloney@gmail.com>
2020-09-12 10:43:36 -0400

    Include the precompiled ESP VM's in browser/Chromebook embedded file systems

--- chromeApp/emscripten/buildEmcc.sh
+++ chromeApp/emscripten/buildEmcc.sh
@@ -13,6 +13,7 @@ cp -r ../../gp/Libraries .
 mkdir precompiled
 cp -r ../../precompiled/*.hex precompiled
 cp -r ../../precompiled/*.uf2 precompiled
+cp -r ../../precompiled/*.bin precompiled
 cp -r ../../gp/runtime .
 cp ../../ide/* runtime/lib
 mv runtime/lib/MicroBlocksPatches.gp runtime/lib/zzzMicroBlocksPatches.gp # makes patches load last
