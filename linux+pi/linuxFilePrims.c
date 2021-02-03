a9116094a9a1084dfdcaff9670659cfa8504b1a0
Bernat Romagosa <bromagosa@e-citilab.eu>
2021-02-03 11:47:10 +0100

    LinuxVM: all file primitives working

--- linux+pi/linuxFilePrims.c
+++ linux+pi/linuxFilePrims.c
@@ -12,82 +12,131 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <sys/statvfs.h>
 #include <dirent.h>
 
 #include "mem.h"
 #include "interp.h"
 
-FILE *currentFilePointer;
-char *currentFileName;
+typedef struct {
+	char fileName[32];
+	FILE *file;
+} FileEntry;
+
+#define FILE_ENTRIES 64
+static FileEntry fileEntry[FILE_ENTRIES]; // records open files
+
 DIR *directory;
 struct dirent *nextDirEntry;
 struct stat fileStat;
+struct statvfs fsStat;
 
-static char *extractFilename(OBJ obj) {
-	char *fileName = "\0";
+static void extractFilename(OBJ obj, char *outputFileName) {
+	outputFileName[0] = '\0';
 	if (IS_TYPE(obj, StringType)) {
-		fileName = obj2str(obj);
-		if (strcmp(fileName, "ublockscode") == 0) return "\0";
+		int size = strlen(obj2str(obj));
+		memcpy(outputFileName, obj2str(obj), size);
+		outputFileName[size] = '\0';
+		if (strcmp(outputFileName, "ublockscode") == 0) {
+			outputFileName[0] = '\0';
+		}
 	} else {
 		fail(needsStringError);
 	}
-	return fileName;
 }
 
-char ensureOpen (char *fileName) {
-	if (strcmp(currentFileName, fileName) != 0) {
-		// currently open file is different from requested one
-		if (currentFilePointer != NULL) {
-			// and there was a currently open file, so we close it first
-			fclose(currentFilePointer);
-		}
-		memcpy(currentFileName, fileName, strlen(fileName));
-		currentFilePointer = fopen(currentFileName, "a+");
-	} else if (fileName[0] && currentFilePointer == NULL) {
-		// current file was closed, so we reopen it
-		currentFilePointer = fopen(currentFileName, "a+");
+static int entryFor(char *fileName) {
+	// Return the index of a file entry for the file with the given path.
+	// Return -1 if fileName doesn't match any entry.
+
+	if (!fileName[0]) return -1; // empty string is not a valid file name
+	for (int i = 0; i < FILE_ENTRIES; i++) {
+		if (0 == strcmp(fileName, fileEntry[i].fileName)) return i;
 	}
-	return currentFilePointer != NULL;
+	return -1;
+}
+
+static int freeEntry() {
+	// Return the index of an unused file entry or -1 if there isn't one.
+
+	for (int i = 0; i < FILE_ENTRIES; i++) {
+		if (!fileEntry[i].file) return i;
+	}
+	return -1; // no free entry
 }
 
 static OBJ primOpen(int argCount, OBJ *args) {
 	if (argCount < 1) return fail(notEnoughArguments);
-	currentFileName = extractFilename(args[0]);
-	if (!currentFileName[0]) return falseObj;
-	currentFilePointer = fopen(currentFileName, "a+");
+	char fileName[100];
+	extractFilename(args[0], fileName);
+	int i = entryFor(fileName);
+	if (i >= 0) { // found an existing entry; close and reopen
+		fclose(fileEntry[i].file);
+		fileEntry[i].file = fopen(fileName, "a+");
+		fseek(fileEntry[i].file, 0, SEEK_SET); // read from start of file
+		return falseObj;
+	}
+	i = freeEntry();
+	if (i >= 0) { // initialize new entry
+		fileEntry[i].fileName[0] = '\0';
+		strncat(fileEntry[i].fileName, fileName, 31);
+		fileEntry[i].file = fopen(fileName, "a+");
+		fseek(fileEntry[i].file, 0, SEEK_SET); // read from start of file
+	}
 	return falseObj;
 }
 
 static OBJ primClose(int argCount, OBJ *args) {
 	if (argCount < 1) return fail(notEnoughArguments);
-	char *fileName = extractFilename(args[0]);
-	if (strcmp(currentFileName, fileName) == 0 &&
-			currentFilePointer != NULL) {
-		fclose(currentFilePointer);
-		currentFilePointer = NULL;
+	char fileName[100];
+	extractFilename(args[0], fileName);
+	int i = entryFor(fileName);
+	if (i >= 0) {
+		fileEntry[i].fileName[0] = '\0';
+		fclose(fileEntry[i].file);
+		return falseObj;
 	}
 	return falseObj;
 }
 
-static OBJ primDelete(int argCount, OBJ *args) { return falseObj; }
+static OBJ primDelete(int argCount, OBJ *args) {
+	if (argCount < 1) return fail(notEnoughArguments);
+	char fileName[100];
+	extractFilename(args[0], fileName);
+
+	int i = entryFor(fileName);
+	if (i >= 0) {
+		fileEntry[i].fileName[0] = '\0';
+		fclose(fileEntry[i].file);
+	}
+
+	if (fileName[0]) remove(fileName);
+	return falseObj;
+}
 
 static OBJ primEndOfFile(int argCount, OBJ *args) {
 	if (argCount < 1) return fail(notEnoughArguments);
-	char *fileName = extractFilename(args[0]);
-	if (!fileName[0] || !ensureOpen(fileName)) return trueObj;
+	char fileName[100];
+	extractFilename(args[0], fileName);
 
-	return feof(currentFilePointer) ? trueObj : falseObj;
+	int i = entryFor(fileName);
+	if (i < 0) return trueObj;
+
+	return feof(fileEntry[i].file) ? trueObj : falseObj;
 }
 
 static OBJ primReadLine(int argCount, OBJ *args) {
 	if (argCount < 1) return fail(notEnoughArguments);
-	char *fileName = extractFilename(args[0]);
-	if (!fileName[0] || !ensureOpen(fileName)) return newString(0);
+	char fileName[100];
+	extractFilename(args[0], fileName);
+
+	int i = entryFor(fileName);
+	if (i < 0) return newString(0);
 
 	char buf[800];
 	uint32 byteCount = 0;
 
-	if (fgets(buf, 800, currentFilePointer) != NULL) {
+	if (fgets(buf, 800, fileEntry[i].file) != NULL) {
 		byteCount = strlen(buf);
 		OBJ result = newString(byteCount);
 		if (result) {
@@ -98,38 +147,66 @@ static OBJ primReadLine(int argCount, OBJ *args) {
 	return newString(0);
 }
 
-static OBJ primReadBytes(int argCount, OBJ *args) { return falseObj; }
+static OBJ primReadBytes(int argCount, OBJ *args) {
+	if (argCount < 2) return fail(notEnoughArguments);
+	if (!isInt(args[0])) return fail(needsIntegerError);
+	uint32 byteCount = obj2int(args[0]);
+	char fileName[100];
+	extractFilename(args[1], fileName);
+
+	int i = entryFor(fileName);
+	if (i >= 0) {
+		uint8 buf[800];
+		if (byteCount > sizeof(buf)) byteCount = sizeof(buf);
+		if ((argCount > 2) && isInt(args[2])) {
+			fseek(fileEntry[i].file, obj2int(args[2]), SEEK_SET);
+		}
+		byteCount = fread(buf, 1, byteCount, fileEntry[i].file);
+		int wordCount = (byteCount + 3) / 4;
+		OBJ result = newObj(ByteArrayType, wordCount, falseObj);
+		if (result) {
+			setByteCountAdjust(result, byteCount);
+			memcpy(&FIELD(result, 0), buf, byteCount);
+			return result;
+		}
+	}
+	return newObj(ByteArrayType, 0 ,falseObj);
+}
 
 static OBJ primAppendLine(int argCount, OBJ *args) {
 	if (argCount < 2) return fail(notEnoughArguments);
 	if (!IS_TYPE(args[1], StringType)) return fail(needsStringError);
-	char *fileName = extractFilename(args[1]);
+	char fileName[100];
+	extractFilename(args[1], fileName);
 	OBJ arg = args[0];
 
-	if (!fileName[0] || !ensureOpen(fileName)) return newString(0);
-
-	if (IS_TYPE(arg, StringType)) {
-		fprintf(currentFilePointer, "%s", obj2str(arg));
-	} else if (isInt(arg)) {
-		fprintf(currentFilePointer, "%i", obj2int(arg));
-	} else if (isBoolean(arg)) {
-		fprintf(currentFilePointer, "%s", (trueObj == arg) ? "true" : "false");
-	} else if (IS_TYPE(arg, ListType)) {
-		// print list items separated by spaces
-		int count = obj2int(FIELD(arg, 0));
-		for (int j = 1; j <= count; j++) {
-			OBJ item = FIELD(arg, j);
-			if (IS_TYPE(item, StringType)) {
-				fprintf(currentFilePointer, "%s", obj2str(item));
-			} else if (isInt(item)) {
-				fprintf(currentFilePointer, "%i", obj2int(item));
-			} else if (isBoolean(item)) {
-				fprintf(currentFilePointer, "%s", (trueObj == item) ? "true" : "false");
+	int i = entryFor(fileName);
+	if (i >= 0) {
+		if (IS_TYPE(arg, StringType)) {
+			fprintf(fileEntry[i].file, "%s", obj2str(arg));
+		} else if (isInt(arg)) {
+			fprintf(fileEntry[i].file, "%i", obj2int(arg));
+		} else if (isBoolean(arg)) {
+			fprintf(fileEntry[i].file, "%s", (trueObj == arg) ?
+					"true" : "false");
+		} else if (IS_TYPE(arg, ListType)) {
+			// print list items separated by spaces
+			int count = obj2int(FIELD(arg, 0));
+			for (int j = 1; j <= count; j++) {
+				OBJ item = FIELD(arg, j);
+				if (IS_TYPE(item, StringType)) {
+					fprintf(fileEntry[i].file, "%s", obj2str(item));
+				} else if (isInt(item)) {
+					fprintf(fileEntry[i].file, "%i", obj2int(item));
+				} else if (isBoolean(item)) {
+					fprintf(fileEntry[i].file, "%s", (trueObj == item) ?
+							"true" : "false");
+				}
+				if (j < count) fprintf(fileEntry[i].file, " ");
 			}
-			if (j < count) fprintf(currentFilePointer, " ");
 		}
+		fprintf(fileEntry[i].file, "\n");
 	}
-	fprintf(currentFilePointer, "\n");
 	processMessage();
 	return falseObj;
 }
@@ -137,14 +214,17 @@ static OBJ primAppendLine(int argCount, OBJ *args) {
 static OBJ primAppendBytes(int argCount, OBJ *args) {
 	if (argCount < 2) return fail(notEnoughArguments);
 	OBJ data = args[0];
-	char *fileName = extractFilename(args[1]);
-	if (fileName[0] && ensureOpen(fileName)) {
-		if (IS_TYPE(data, ByteArrayType)) {
-			fwrite((uint8 *) &FIELD(data, 0), 8, BYTES(data), currentFilePointer);
-		} else if (IS_TYPE(data, StringType)) {
-			char *s = obj2str(data);
-			fwrite((uint8 *) s, 8, strlen(s), currentFilePointer);
-		}
+	char fileName[100];
+	extractFilename(args[1], fileName);
+
+	int i = entryFor(fileName);
+	if (i < 0) return falseObj;
+
+	if (IS_TYPE(data, ByteArrayType)) {
+		fwrite((uint8 *) &FIELD(data, 0), 1, BYTES(data), fileEntry[i].file);
+	} else if (IS_TYPE(data, StringType)) {
+		char *s = obj2str(data);
+		fwrite((uint8 *) s, 1, strlen(s), fileEntry[i].file);
 	}
 	processMessage();
 	return falseObj;
@@ -152,8 +232,9 @@ static OBJ primAppendBytes(int argCount, OBJ *args) {
 
 static OBJ primFileSize(int argCount, OBJ *args) {
 	if (argCount < 1) return fail(notEnoughArguments);
-	char *fileName = extractFilename(args[0]);
-	if (!fileName[0] || !ensureOpen(fileName)) return int2obj(0);
+	char fileName[100];
+	extractFilename(args[0], fileName);
+	if (!fileName[0]) return int2obj(0);
 	struct stat st;
 	stat(fileName, &st);
 	return int2obj(st.st_size);
@@ -185,7 +266,14 @@ static OBJ primNextFileInList(int argCount, OBJ *args) {
 	return newStringFromBytes(fileName, strlen(fileName));
 }
 
-static OBJ primSystemInfo(int argCount, OBJ *args) { return falseObj; }
+static OBJ primSystemInfo(int argCount, OBJ *args) {
+	char result[100];
+	statvfs(".", &fsStat);
+	int freeBytes = fsStat.f_bavail * fsStat.f_frsize;
+	int totalBytes = fsStat.f_blocks * fsStat.f_frsize;
+	sprintf(result, "%u bytes used of %u", totalBytes - freeBytes, totalBytes);
+	return newStringFromBytes(result, strlen(result));
+}
 
 // Primitives
 
