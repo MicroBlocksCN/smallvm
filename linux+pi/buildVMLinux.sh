a517b9917df40e5433156f3126fdc7b447fc2db5
John Maloney <jhmaloney@gmail.com>
2021-02-12 17:41:57 -0500

    Rename .cpp files to .c (we don't need C++)

--- linux+pi/buildVMLinux.sh
+++ linux+pi/buildVMLinux.sh
@@ -7,8 +7,8 @@
 #
 # Prereq for 64-bit Linux systems: libc6-dev-i386 gcc-multilib g++-multilib libsdl2-dev:i386 libsdl2-ttf-dev:i386
 
-gcc -m32 -std=c99 -Wall -O3 linuxIOPrims.cpp linuxTftPrims.cpp -lSDL2 -lSDL2_ttf -lm \
+gcc -m32 -std=c99 -Wall -O3 linuxIOPrims.c linuxTftPrims.c -lSDL2 -lSDL2_ttf -lm \
 	-D GNUBLOCKS -I ../vm linux.c linux.h \
-	linuxFilePrims.c linuxNetPrims.c ../vm/*.c ../vm/outputPrims.cpp \
+	linuxFilePrims.c linuxNetPrims.c ../vm/*.c ../vm/outputPrims.c \
 	-lm \
 	-o GnuBlocks
