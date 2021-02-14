5c72133083bdc94c2c3ddb7195138ea9b16c8ccf
John Maloney <jhmaloney@gmail.com>
2021-02-13 19:51:58 -0500

    Updated build script

--- linux+pi/buildVMLinux.sh
+++ linux+pi/buildVMLinux.sh
@@ -7,8 +7,15 @@
 #
 # Prereq for 64-bit Linux systems: libc6-dev-i386 gcc-multilib g++-multilib libsdl2-dev:i386 libsdl2-ttf-dev:i386
 
-gcc -m32 -std=c99 -Wall -O3 linuxIOPrims.c linuxTftPrims.c -lSDL2 -lSDL2_ttf -lm \
-	-D GNUBLOCKS -I ../vm linux.c linux.h \
-	linuxFilePrims.c linuxNetPrims.c ../vm/*.c ../vm/outputPrims.c \
-	-lm \
+gcc -m32 -std=c99 -Wall -Wno-unused-variable -Wno-unused-result -O3 \
+	-D GNUBLOCKS \
+	-I/usr/local/include/SDL2 \
+	-I ../vm \
+	linux.c ../vm/*.c \
+	linuxFilePrims.c linuxIOPrims.c linuxNetPrims.c linuxOutputPrims.c linuxTftPrims.c \
+	-lSDL2 -lSDL2_ttf \
+	-ldl -lm -lpthread -lsndio -lz -lpng \
 	-o GnuBlocks
+
+# For static linking:
+#	/usr/local/lib/libSDL2.a /usr/local/lib/libSDL2_ttf.a /usr/local/lib/libfreetype.a \
