0183b0d498c28f18b87dba15646ad67f61a955d5
John Maloney <jhmaloney@gmail.com>
2021-04-04 13:22:02 -0400

    Tweak comments about dependencies Added comments about how libraries were built

--- linux+pi/buildVMLinux.sh
+++ linux+pi/buildVMLinux.sh
@@ -3,15 +3,15 @@
 # Connect to it via pseudo terminal
 #
 # Prerequisites to run on 64-bit Linux (tested on Ubuntu 20.04):
-#	sudo apt install gcc-multilib libgl1-mesa-glx:i386
-#
-# Might be needed: libasound2:i386
+#	sudo apt install gcc-multilib libgl1-mesa-glx:i386 libasound2:i386
 #
 # Additional prerequisites to build GnuBlocks on a 64-bit Linux system:
 #	sudo apt install libsdl2-dev:i386 libsdl2-ttf-dev:i386 libpng-dev:i386
 #
-# Not needed in John's test:
-#	libc6-dev-i386 g++-multilib zlib1g-dev:i386
+# Libraries built on Ubutu 16.04 (32-bit):
+#	SDL2-2.0.12 (./configure --disable-video-vulkan --disable-video-opengles --enable-alsa --disable-alsa-shared)
+#	SDL2-ttf-2.0.15 (default configuration)
+#	freetype-2.10.4 (default configuration)
 
 gcc -m32 -std=c99 -Wall -Wno-unused-variable -Wno-unused-result -O3 \
 	-D GNUBLOCKS \
