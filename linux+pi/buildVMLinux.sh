15ae13c816c2ef89fd43e125e479427586135a3c
John Maloney <jhmaloney@gmail.com>
2021-04-23 16:16:16 -0400

    Updated precompiled SDL2 library to include sndio option

--- linux+pi/buildVMLinux.sh
+++ linux+pi/buildVMLinux.sh
@@ -9,7 +9,8 @@
 #	sudo apt install libsdl2-dev:i386 libsdl2-ttf-dev:i386 libpng-dev:i386
 #
 # Libraries built on Ubutu 16.04 (32-bit):
-#	SDL2-2.0.12 (./configure --disable-video-vulkan --disable-video-opengles --disable-alsa-shared --disable-pulseaudio)
+#	Prerequisite: libsndio-dev (without this installed, sndio won't be included)
+#	SDL2-2.0.12 (./configure --disable-video-vulkan --disable-video-opengles --enable-sndio)
 #	SDL2-ttf-2.0.15 (default configuration)
 #	freetype-2.10.4 (default configuration)
 
