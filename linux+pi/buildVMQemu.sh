14ee91a9678b66a04bc5e1250f16cc60d1a98191
Bernat Romagosa <bromagosa@e-citilab.eu>
2021-05-03 15:03:30 +0200

    do not attempt to build LinuxVM w/Qemu if missing image file

--- linux+pi/buildVMQemu.sh
+++ linux+pi/buildVMQemu.sh
@@ -2,6 +2,6 @@ branch=`git branch --show-current`
 echo "user and password are both 'gnublocks'"
 echo "cd smallvm/linux+pi; git fetch; git reset --hard origin/$branch; git pull; ./buildVMLinux.sh; cp vm.linux.i386 /host; shutdown -h now" > /tmp/vm-script.sh
 chmod +x /tmp/vm-script.sh
-qemu-system-x86_64 -curses -m 512 -hda ubuntu16.04.img -virtfs local,path=/tmp,mount_tag=host0,security_model=passthrough,id=host0
+qemu-system-x86_64 -curses -m 512 -hda ubuntu16.04.img -virtfs local,path=/tmp,mount_tag=host0,security_model=mapped-xattr,id=host0 -k es
 mv /tmp/vm.linux.i386 .
 rm /tmp/vm-script.sh
