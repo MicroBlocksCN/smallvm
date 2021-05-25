3f1d1f2cfce729e96167fe5021f98ff3b7c33231
Bernat Romagosa <bromagosa@e-citilab.eu>
2021-05-25 13:35:39 +0200

    buildVMQemu script builds for both RaspberryPi and Linux

--- linux+pi/buildVMQemu.sh
+++ linux+pi/buildVMQemu.sh
@@ -1,7 +1,47 @@
-branch=`git branch --show-current`
-echo "user and password are both 'gnublocks'"
-echo "cd smallvm/linux+pi; git fetch; git reset --hard origin/$branch; git pull; ./buildVMLinux.sh; cp vm.linux.i386 /host; shutdown -h now" > /tmp/vm-script.sh
-chmod +x /tmp/vm-script.sh
-qemu-system-x86_64 -nographic -m 512 -hda ubuntu16.04.img -virtfs local,path=/tmp,mount_tag=host0,security_model=mapped-xattr,id=host0 -k es
-mv /tmp/vm.linux.i386 .
-rm /tmp/vm-script.sh
+#!/bin/sh
+# Build VMs for Linux and RaspberryPi OS under their respective Qemu images
+
+# Remove virtual disk if it exists
+rm -f qemu/host.img
+
+# Create an empty virtual disk image
+qemu-img create qemu/host.img 500M
+mkfs.ext2 qemu/host.img
+
+# Mount it
+mkdir -p /tmp/host
+fuseext2 -o rw+ qemu/host.img /tmp/host
+
+# Copy necessary files
+mkdir -p /tmp/host/linux+pi
+mkdir -p /tmp/host/linux+pi/libs
+cp * /tmp/host/linux+pi
+cp libs/* /tmp/host/linux+pi/libs
+cp -r ../vm /tmp/host
+
+# Create build scripts
+echo "cd /host/linux+pi; ./buildVMRaspberryPi.sh; sudo shutdown -h now" > /tmp/host/raspi-script.sh
+echo "cd /host/linux+pi; ./buildVMLinux.sh; shutdown -h now" > /tmp/host/linux-script.sh
+chmod +x /tmp/host/raspi-script.sh
+chmod +x /tmp/host/linux-script.sh
+
+# Unmount the virtual disk image
+fusermount -u /tmp/host
+
+# Launch Qemu virtual machines.
+# They will automatically mount the host image, log in and run their respective build scripts.
+
+# remove -nographic from both when not in deploy server
+
+### Launch RaspberryPi Qemu VM ###
+qemu-system-arm -kernel qemu/kernel-qemu-4.4.34-jessie -cpu arm1176 -m 256 -M versatilepb -k es -append "panic=-1 root=/dev/sda2 rootfstype=ext4 rw" -hda qemu/raspbian-jessie-lite.img -hdb qemu/host.img -no-reboot -nographic
+
+### Launch Linux Qemu VM ###
+qemu-system-x86_64 -m 512 -hda qemu/ubuntu16.04.img -k es -hdb qemu/host.img -nographic
+
+# Mount the virtual disk image again
+fuseext2 -o rw+ qemu/host.img /tmp/host
+# Copy the compiled MicroBlocks VMs out of the virtual disk image
+cp /tmp/host/linux+pi/vm.linux.* .
+# Unmount the virtual disk again
+fusermount -u /tmp/host
