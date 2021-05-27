d002a2d40340bb1f766b9729ca02efe159860d78
John Maloney <jhmaloney@gmail.com>
2021-05-27 05:54:17 -0400

    Clean up RaspberryPi build script.

--- linux+pi/buildVMRaspberryPi.sh
+++ linux+pi/buildVMRaspberryPi.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # Build uBlocks on the Raspberry Pi
 #
-# Prereqs for raspbian systems:
+# Prereqs for Raspbian ("Buster") system:
 #	libsdl2-dev libsdl2-ttf-dev
-# John did not need to install these (on newly installed Raspberrian "Buster"):
-#	wiringpi libc6-dev gcc g++
 
 gcc -std=c99 -O3 -Wall -Wno-unused-variable -Wno-unused-result \
 	-D GNUBLOCKS \
@@ -18,6 +16,3 @@ gcc -std=c99 -O3 -Wall -Wno-unused-variable -Wno-unused-result \
 	-l wiringPi \
 	-ldl -lm -lpthread -lsndio -lz \
 	-o vm.linux.rpi
-
-# For static linking replace -lSDL2 -lSDL2_ttf with:
-#	/usr/local/lib/libSDL2.a /usr/local/lib/libSDL2_ttf.a /usr/local/lib/libfreetype.a \
