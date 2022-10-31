ab6327d30353d431f0be0af5848fcbd36a2aca58
Bernat Romagosa <bromagosa@e-citilab.eu>
2022-10-31 23:56:47 +0100

    boardie: all TFT prims except rounded rects working

--- boardie/boardieTftPrims.c
+++ boardie/boardieTftPrims.c
@@ -15,47 +15,35 @@
 #include "mem.h"
 #include "interp.h"
 
-#define DEFAULT_WIDTH 320
-#define DEFAULT_HEIGHT 240
-#define REFRESH_MSECS 16 // screen refresh interval (~60 frames/sec)
-
-static int mouseDown = false;
-static int mouseX = -1;
-static int mouseY = -1;
-static int mouseDownTime = 0;
+#define DEFAULT_WIDTH 200
+#define DEFAULT_HEIGHT 150
 
 static int tftEnabled = false;
 static int lastRefreshTime = 0;
 
-static int ttfInitialized = false;
-
-// Helpers
-
-static int decodeColor (int color24b) {
-	return (color24b >> 16) & 255, (color24b >> 8) & 255, color24b & 255, 255;
-}
-
-
 void tftClear() {
 	tftInit();
+	EM_ASM_({
+		ctx.clearRect(0, 0, $0, $1);
+	}, DEFAULT_WIDTH, DEFAULT_HEIGHT);
 }
 
 void tftInit() {
 	if (!tftEnabled) {
 		lastRefreshTime = millisecs();
 		tftEnabled = true;
+
+		EM_ASM_({
+			window.rgbFrom24b = function (color24b) {
+				return 'rgb(' + ((color24b >> 16) & 255) + ',' +
+					((color24b >> 8) & 255) + ',' + (color24b & 255) + ')';
+			};
+		}, NULL);
 	}
 }
 
 void updateMicrobitDisplay() {
-	if (tftEnabled) {
-		uint32_t now = millisecs();
-		if (now < lastRefreshTime) lastRefreshTime = 0; // clock wrap
-		if ((now - lastRefreshTime > REFRESH_MSECS)) {
-			// do the rendering
-			lastRefreshTime = now;
-		}
-	}
+	// do nothing?
 }
 
 // TFT Primitives
@@ -80,58 +68,81 @@ static OBJ primGetHeight(int argCount, OBJ *args) {
 
 static OBJ primSetPixel(int argCount, OBJ *args) {
 	tftInit();
-	int x = obj2int(args[0]);
-	int y = obj2int(args[1]);
-	// paint the pixel
+	EM_ASM_({
+			ctx.fillStyle = rgbFrom24b($2);
+			ctx.fillRect($0, $1, 1, 1);
+		},
+		obj2int(args[0]), // x
+		obj2int(args[1]), // y
+		obj2int(args[2]) // color
+	);
 	return falseObj;
 }
 
 static OBJ primLine(int argCount, OBJ *args) {
 	tftInit();
-	int x0 = obj2int(args[0]);
-	int y0 = obj2int(args[1]);
-	int x1 = obj2int(args[2]);
-	int y1 = obj2int(args[3]);
-	// paint the line
+	EM_ASM_({
+			ctx.strokeStyle = rgbFrom24b($4);
+			ctx.beginPath();
+			ctx.moveTo($0, $1);
+			ctx.lineTo($2, $3);
+			ctx.stroke();
+		},
+		obj2int(args[0]), // x0
+		obj2int(args[1]), // y0
+		obj2int(args[2]), // x1
+		obj2int(args[3]), // y1
+		obj2int(args[4]) // color
+	);
 	return falseObj;
 }
 
 static OBJ primRect(int argCount, OBJ *args) {
 	tftInit();
-
 	EM_ASM_({
-			var x = $0;
-			var y = $1;
-			var width = $2;
-			var height = $3;
-			var fill = $5;
-			var ctx = document.querySelector('#screen').getContext('2d');
-			if (fill) {
-				ctx.fillStyle = $4; // not working
-				ctx.fillRect(x, y, width, height);
+			if ($5) {
+				ctx.fillStyle = rgbFrom24b($4);
+				ctx.fillRect($0, $1, $2, $3);
 			} else {
-				ctx.strokeStyle = $4; // not working
-				ctx.rect(x, y, width, height);
-				ctx.stroke();
+				ctx.strokeStyle = rgbFrom24b($4);
+				ctx.strokeRect($0, $1, $2, $3);
 			}
 		},
-		obj2int(args[0]),
-		obj2int(args[1]),
-		obj2int(args[2]),
-		obj2int(args[3]),
-		decodeColor(obj2int(args[4])),
-		(argCount > 5) ? (trueObj == args[5]) : true);
+		obj2int(args[0]), // x
+		obj2int(args[1]), // y
+		obj2int(args[2]), // width
+		obj2int(args[3]), // height
+		obj2int(args[4]), // color
+		(argCount > 5) ? (trueObj == args[5]) : true // fill
+	);
 	return falseObj;
 }
 
 static OBJ primCircle(int argCount, OBJ *args) {
 	tftInit();
-	int originX = obj2int(args[0]);
-	int originY = obj2int(args[1]);
-	int radius = obj2int(args[2]);
-	//setRenderColor(obj2int(args[3]));
-	int fill = (argCount > 4) ? (trueObj == args[4]) : true;
-	// draw circle
+	EM_ASM_({
+			var x = $0;
+			var y = $1;
+			var r = $2;
+			var fill = $4;
+			if (fill) {
+				ctx.fillStyle = rgbFrom24b($3);
+				ctx.beginPath();
+				ctx.arc(x, y, r, 0, 2 * Math.PI);
+				ctx.fill();
+			} else {
+				ctx.strokeStyle = rgbFrom24b($3);
+				ctx.beginPath();
+				ctx.arc(x, y, r, 0, 2 * Math.PI);
+				ctx.stroke();
+			}
+		},
+		obj2int(args[0]), // x
+		obj2int(args[1]), // y
+		obj2int(args[2]), // radius
+		obj2int(args[3]), // color
+		(argCount > 4) ? (trueObj == args[4]) : true // fill
+	);
 	return falseObj;
 }
 
@@ -163,27 +174,41 @@ static OBJ primTriangle(int argCount, OBJ *args) {
 	tftInit();
 
 	// draw triangle
-	/*
-			obj2int(args[0]),
-			obj2int(args[1]),
-			obj2int(args[2]),
-			obj2int(args[3]),
-			obj2int(args[4]),
-			obj2int(args[5])
-	*/
+	EM_ASM_({
+			if ($7) {
+				ctx.fillStyle = rgbFrom24b($6);
+			} else {
+				ctx.strokeStyle = rgbFrom24b($6);
+			}
+			ctx.beginPath();
+			ctx.moveTo($0, $1);
+			ctx.lineTo($2, $3);
+			ctx.lineTo($3, $4);
+			ctx.closePath();
+			if ($7) {
+				ctx.fill();
+			} else {
+				ctx.stroke();
+			}
+		},
+		obj2int(args[0]), // x0
+		obj2int(args[1]), // y0
+		obj2int(args[2]), // x1
+		obj2int(args[3]), // y1
+		obj2int(args[4]), // x2
+		obj2int(args[5]), // y2
+		obj2int(args[6]), // color
+		(argCount > 7) ? (trueObj == args[7]) : true // fill
+	);
 
 	return falseObj;
 }
 
 static OBJ primText(int argCount, OBJ *args) {
+	// TODO wrap using measureText. See: https://stackoverflow.com/a/16599668
 	tftInit();
 	OBJ value = args[0];
 	char text[256];
-	int x = obj2int(args[1]);
-	int y = obj2int(args[2]);
-	int color24b = obj2int(args[3]);
-	int scale = (argCount > 4) ? obj2int(args[4]) : 2;
-	int wrap = (argCount > 5) ? (trueObj == args[5]) : true;
 
 	if (IS_TYPE(value, StringType)) {
 		sprintf(text, "%s", obj2str(value));
@@ -196,6 +221,19 @@ static OBJ primText(int argCount, OBJ *args) {
 	}
 
 	// draw text
+	EM_ASM_({
+			ctx.fillStyle = rgbFrom24b($3);
+			ctx.font = ($4 * 6) + 'px monospace';
+			ctx.textBaseline = 'top';
+			ctx.fillText(UTF8ToString($0), $1, $2);
+		},
+		text, // text
+		obj2int(args[1]), // x
+		obj2int(args[2]), // y
+		obj2int(args[3]), // color
+		(argCount > 4) ? obj2int(args[4]) : 2, // scale
+		(argCount > 5) ? (trueObj == args[5]) : true // wrap
+	);
 
 	return falseObj;
 }
@@ -205,18 +243,26 @@ static OBJ primText(int argCount, OBJ *args) {
 void tftSetHugePixel(int x, int y, int state) {
 	// simulate a 5x5 array of square pixels like the micro:bit LED array
 	tftInit();
+	int minDimension, xInset = 0, yInset = 0;
+	if (DEFAULT_WIDTH > DEFAULT_HEIGHT) {
+		minDimension = DEFAULT_HEIGHT;
+		xInset = (DEFAULT_WIDTH - DEFAULT_HEIGHT) / 2;
+	} else {
+		minDimension = DEFAULT_WIDTH;
+		yInset = (DEFAULT_HEIGHT - DEFAULT_WIDTH) / 2;
+	}
+	int lineWidth = (minDimension > 60) ? 3 : 1;
+	int squareSize = (minDimension - (6 * lineWidth)) / 5;
 
-	int lineWidth = 1;
-	int squareSize = (DEFAULT_WIDTH + 3) / 5;
-
-	//setRenderColor(state ? 0x00FF00 : 0x000000);
-
-//	draw rectangle:
-/*		xInset + ((x - 1) * squareSize) + (x * lineWidth),
-		yInset + ((y - 1) * squareSize) + (y * lineWidth),
+	EM_ASM_({
+			ctx.fillStyle = $3 == 0 ? '#000' : '#0F0';
+			ctx.fillRect($0, $1, $2, $2);
+		},
+		xInset + ((x - 1) * squareSize) + (x * lineWidth), // x
+		yInset + ((y - 1) * squareSize) + (y * lineWidth), // y
 		squareSize,
-		squareSize,
-	*/
+		state
+	);
 }
 
 void tftSetHugePixelBits(int bits) {
