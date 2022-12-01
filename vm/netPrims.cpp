a6dfb1ad759c1101c437c5f570531ac50a39acb6
CWEIB <44942392+CWEIB@users.noreply.github.com>
2022-12-02 00:46:10 +0800

    增加BLE停止蓝牙广播功能

--- vm/netPrims.cpp
+++ vm/netPrims.cpp
@@ -1068,6 +1068,14 @@ static OBJ primBLE_UART_Write(int argCount, OBJ *args) {
 	return falseObj;
 }
 
+static OBJ primBLE_UART_ServerStop(int argCount, OBJ *args)
+{
+	BLEDevice::stopAdvertising();
+	BLEDevice::deinit(false);
+	BLEServerStarted = false;
+	return falseObj;
+}
+
 #endif // BLE_PRIMS
 
 // Experimental! Optional ESP Now support (compile with -D ESP_NOW_PRIMS)
@@ -1171,6 +1179,7 @@ static PrimEntry entries[] = {
 
   #if defined(BLE_PRIMS)
 	{"BLE_UART_ServerStart", primBLE_UART_ServerStart},
+	{"BLE_UART_ServerStop", primBLE_UART_ServerStop},
 	{"BLE_DeviceConnected", primBLE_DeviceConnected},
 	{"BLE_UART_LastEvent", primBLE_UART_LastEvent},
 	{"BLE_UART_Write", primBLE_UART_Write},
