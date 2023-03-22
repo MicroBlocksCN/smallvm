6ec23eeeef8c9f3e496791daae76777ce12a782c
John Maloney <jhmaloney@gmail.com>
2023-03-22 11:00:54 -0400

    Tweak pin counts for Metro M0 to allow use of analog pins for digital I/O.

--- vm/ioPrims.cpp
+++ vm/ioPrims.cpp
@@ -388,10 +388,10 @@ void restartSerial() {
 #elif defined(ADAFRUIT_METRO_M0_EXPRESS) // must come before Zero
 
 	#define BOARD_TYPE "Metro M0"
-	#define DIGITAL_PINS 14
-	#define ANALOG_PINS 12
-	#define TOTAL_PINS (DIGITAL_PINS + ANALOG_PINS)
-	static const int analogPin[] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11};
+	#define DIGITAL_PINS 20
+	#define ANALOG_PINS 6
+	#define TOTAL_PINS 20
+	static const int analogPin[] = {A0, A1, A2, A3, A4, A5};
 
 #elif defined(ARDUINO_SAMD_ZERO)
 
