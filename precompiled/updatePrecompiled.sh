546bcae70f9b711a65120903da0a69a942a45755
John Maloney <jhmaloney@gmail.com>
2023-04-09 09:47:10 -0400

    Don't build precompiled VM for ItsyBitsy Remove micro:bit V1 and V2 hex files after building the universal hex file Don't copy 'node_modules' subfolder of precompiled folder into the embedded files system These changes save about 400k in the applications.

--- precompiled/updatePrecompiled.sh
+++ precompiled/updatePrecompiled.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Rebuild and update the precompiled binaries.
-rm *.hex *.bin *.uf2
+rm -f *.hex *.bin *.uf2
 cd ..
 pio run -e microbit
 cp .pio/build/microbit/firmware.hex precompiled/vm_microbitV1.hex
@@ -22,8 +22,8 @@ pio run -e cplay52
 python precompiled/uf2conv.py -c .pio/build/cplay52/firmware.hex -f 0xADA52840 -o precompiled/vm_cplay52.uf2
 pio run -e clue
 python precompiled/uf2conv.py -c -f 0xada52840 .pio/build/clue/firmware.hex -o precompiled/vm_clue.uf2
-pio run -e itsybitsy
-python precompiled/uf2conv.py -c .pio/build/itsybitsy/firmware.bin -o precompiled/vm_itsybitsy.uf2
+# pio run -e itsybitsy
+# python precompiled/uf2conv.py -c .pio/build/itsybitsy/firmware.bin -o precompiled/vm_itsybitsy.uf2
 pio run -e metroM0
 python precompiled/uf2conv.py -c .pio/build/metroM0/firmware.bin -o precompiled/vm_metroM0.uf2
 pio run -e pico
@@ -47,4 +47,5 @@ cp linux+pi/vm_* precompiled/
 cd precompiled
 npm install
 node buildUniversalHex.js
+rm vm_microbitV1.hex vm_microbitV2.hex
 
