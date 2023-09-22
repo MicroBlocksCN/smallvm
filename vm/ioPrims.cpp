52faf7a34914440f513844277040824a70156d73
Sean Shao <yue.shao@gmail.com>
2023-09-22 11:21:09 +0800

    add cocorobo and matrix board
    添加 cocorobot 板子
    引脚映射，板载传感器陀螺仪，光线，温度，湿度
    温度湿度是用到了额外的i2c引脚，所以需要写到vm 中
    matrix
    添加了引脚映射和光线传感器

--- vm/ioPrims.cpp
+++ vm/ioPrims.cpp
@@ -679,7 +679,38 @@ void hardwareInit() {
 		1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
 		1, 0, 0, 0, 1, 0, 0, 0, 1, 1,
 		1, 1, 0, 0, 0, 0, 0, 1, 1, 0};
+#elif defined(MATRIXBIT)
+	#define BOARD_TYPE "Matrix:bit"
+	#define DIGITAL_PINS 40
+	#define ANALOG_PINS 16
+	#define TOTAL_PINS 40
+	static const int analogPin[] = {};
+	#define PIN_LED 12
+	#define PIN_BUTTON_A 0
+	#define PIN_BUTTON_B 2
+	#define DEFAULT_TONE_PIN 16
+	static const char reservedPin[TOTAL_PINS] = {
+		0, 1, 0, 1, 0, 0, 1, 1, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 0, 0, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 1, 1, 0};
 
+#elif defined(COCOROBO)
+	#define BOARD_TYPE "cocorobo"
+	#define DIGITAL_PINS 40
+	#define ANALOG_PINS 16
+	#define TOTAL_PINS 40
+	static const int analogPin[] = {};
+	#define PIN_LED 12
+	#define PIN_BUTTON_A 34
+	#define PIN_BUTTON_B 35
+	#define DEFAULT_TONE_PIN 26
+	static const char reservedPin[TOTAL_PINS] = {
+		0, 1, 0, 1, 0, 0, 1, 1, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 0, 0, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 1, 1, 0};
+		
 #elif defined(M5STAMP)
 	#define BOARD_TYPE "M5STAMP"
 	#define DIGITAL_PINS 22
