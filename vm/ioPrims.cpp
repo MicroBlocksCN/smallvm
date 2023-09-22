46abf575d14e36bb444768b6fcaf1472c6a39fed
Sean Shao <yue.shao@gmail.com>
2023-09-22 11:21:09 +0800

    add cocorobo and matrix board
    添加 cocorobot 板子
    引脚映射，板载传感器陀螺仪，光线，温度，湿度
    温度湿度是用到了额外的i2c引脚，所以需要写到vm 中
    matrix
    添加了引脚映射和光线传感器

--- vm/ioPrims.cpp
+++ vm/ioPrims.cpp
@@ -839,7 +839,7 @@ extern "C" void esp8266DeepSleep(uint64_t usecs) {
 	#define ANALOG_PINS 16
 	#define TOTAL_PINS 40
 	static const int analogPin[] = {};
-	#define PIN_LED 14
+	#define PIN_LED 12
 	#define PIN_BUTTON_A 34
 	#define PIN_BUTTON_B 35
 	#define DEFAULT_TONE_PIN 26
