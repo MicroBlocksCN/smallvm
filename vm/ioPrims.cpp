9e9da6f9984f64869c388497fecc92fcdfc94d48
Sean Shao <yue.shao@gmail.com>
2023-10-10 14:11:03 +0800

    add cocorobo drive lib and turn off leds

--- vm/ioPrims.cpp
+++ vm/ioPrims.cpp
@@ -839,15 +839,15 @@ extern "C" void esp8266DeepSleep(uint64_t usecs) {
 	#define ANALOG_PINS 16
 	#define TOTAL_PINS 40
 	static const int analogPin[] = {};
-	#define PIN_LED 12
+	#define PIN_LED 14
 	#define PIN_BUTTON_A 34
 	#define PIN_BUTTON_B 35
 	#define DEFAULT_TONE_PIN 26
 	static const char reservedPin[TOTAL_PINS] = {
-		0, 1, 0, 1, 0, 0, 1, 1, 1, 1,
-		1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-		1, 0, 0, 0, 1, 0, 0, 0, 1, 1,
-		1, 1, 0, 0, 0, 0, 0, 1, 1, 0};
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		
 #elif defined(M5STAMP)
 	#define BOARD_TYPE "M5STAMP"
