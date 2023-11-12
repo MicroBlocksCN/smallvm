25bfe1cf9b8a33f233130db63676c41b67edb93f
John Maloney <jhmaloney@gmail.com>
2023-11-11 20:16:05 -0500

    Enable use of service worker to support offline use of the webapp

--- chromeApp/webapp/gpSupport.js
+++ chromeApp/webapp/gpSupport.js
@@ -1185,9 +1185,8 @@ window.onbeforeunload = function() {
 
 // progressive web app service worker
 
-// window.onload = function() {
-//   if (('serviceWorker' in navigator) && !hasChromeFilesystem()) {
-//     navigator.serviceWorker.register('sw.js');
-//   }
-// }
-//
+window.onload = function() {
+  if (('serviceWorker' in navigator) && !hasChromeFilesystem()) {
+    navigator.serviceWorker.register('sw.js');
+  }
+}
