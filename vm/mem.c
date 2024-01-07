3616c1f2f6bdfd8d1e9658d4791fa09e815b7419
wwj718 <wuwenjie718@gmail.com>
2024-01-07 12:15:51 +0800

    添加 ARDUINO_M5STACK_FIRE 的 mem.c 配置

--- vm/mem.c
+++ vm/mem.c
@@ -47,6 +47,8 @@
   #define OBJSTORE_BYTES 160000 // max is 219000
 #elif defined(ARDUINO_ARCH_SAMD)
   #define OBJSTORE_BYTES 14000
+#elif defined(ARDUINO_M5STACK_FIRE)
+  #define OBJSTORE_BYTES 200000 // will be allocated from PSRAM
 #elif defined(HAS_CAMERA)
   #define OBJSTORE_BYTES 240000 // will be allocated from PSRAM
 #elif defined(ESP32_S3) || defined(ESP32_C3)
