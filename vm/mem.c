7851d0f037711984a82ae4b042b0ff8258468d16
Sean Shao <yue.shao@gmail.com>
2024-01-14 13:51:48 +0800

    添加halo 板子，修改内存分配
    添加m5frie halo 板子编译配置

--- vm/mem.c
+++ vm/mem.c
@@ -47,7 +47,7 @@
   #define OBJSTORE_BYTES 160000 // max is 219000
 #elif defined(ARDUINO_ARCH_SAMD)
   #define OBJSTORE_BYTES 14000
-#elif defined(ARDUINO_M5STACK_FIRE)
+#elif defined(ARDUINO_M5STACK_FIRE) || defined(ARDUINO_M5STACK_Core2) || defined(HALOCODE) 
   #define OBJSTORE_BYTES 200000 // will be allocated from PSRAM
 #elif defined(HAS_CAMERA)
   #define OBJSTORE_BYTES 240000 // will be allocated from PSRAM
