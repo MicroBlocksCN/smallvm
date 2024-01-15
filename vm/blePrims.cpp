fa595deaf065a3f412ee63e7556d77466004ea27
John Maloney <jhmaloney@gmail.com>
2024-01-15 09:02:41 -0500

    Tweaks to Octo primitives to improve performance and fix some rare error cases:   - ignore Octo messages with truncated devices names (rare, but I've seen it)   - suppress duplicates using a list of recently seen ID's using "least recently added" replacement policy   (the original implementation based on "set" would sometimes allow duplicates after it filled up because the last item in the hash table was not necessary the oldest -- and in some cases it was actually the message currently being received)   - convert 8-byte Octo ID strings (first 8-bytes of the name) into long long ints for fast equality comparison

--- vm/blePrims.cpp
+++ vm/blePrims.cpp
@@ -135,13 +135,12 @@ static OBJ primBLE_UART_Write(int argCount, OBJ *args) {
 
 #endif // BLE_UART
 
-#if defined(BLE_IDE) // Include new Octo primtives in standard release
+#if defined(BLE_IDE) //Octo primtives; included in standard BLE release
 
 #include <NimBLEDevice.h>
-#include <set>
 
-static const char* OCTO_UUID_Android	= "2540b6b0-0001-4538-bcd7-7ecfb51297c1";
-static const char* OCTO_UUID_iOS		= "2540b6b0-0002-4538-bcd7-7ecfb51297c1";
+NimBLEUUID ANDROID_OCTO_UUID	= NimBLEUUID("2540b6b0-0001-4538-bcd7-7ecfb51297c1");
+NimBLEUUID iOS_OCTO_UUID		= NimBLEUUID("2540b6b0-0002-4538-bcd7-7ecfb51297c1");
 
 static BLEScan* pOctoScanner = NULL;
 static BLEAdvertising* pAdvertising = NULL;
@@ -155,49 +154,69 @@ static int shape_id = 0;
 static int lastScanPayloadLen = 0;
 static uint8 *lastScanPayload[MAX_SCAN_PAYLOAD];
 
-// list of rececently receive datas used for duplicate suppression
-static std::set<std::string> receivedOctoDatas;
-static uint32 receivedOctoDatasMaxLength = 100;
+// octoIDHistory is an array of recently seen Octo ID's used for duplicate suppression.
+// Its size must be a power of 2. Searching for an ID starts at searchStartIndex.
+// When an ID is not found, searchStartIndex is decremented (mod the array size)
+// and the new ID is added at that index. Thus, the most recently added ID
+// is at searchStartIndex, the second most recent at searchStartIndex+1, etc.
+// The first time an ID is seen, the entire history must be searched but after
+// that, the ID will be found in one or, at most, a few steps. The common case
+// is testing for an ID that has already been seen since OctoStudio often sends
+// the same message 50-60 times.
 
-static void recordOctoData(std::string data) {
-	// Record the given string to suppress duplicates.
-	// This is needed because each adveristment is repeated many times.
+typedef long long unsigned int octoMsgID;
+octoMsgID allZeroMessageID;
 
-	receivedOctoDatas.insert(data);
-	if (receivedOctoDatas.size() > receivedOctoDatasMaxLength) {
-		receivedOctoDatas.erase(receivedOctoDatas.begin());
+#define OCTO_ID_HISTORY_SIZE 32
+octoMsgID octoIDHistory[OCTO_ID_HISTORY_SIZE];
+int searchStartIndex = 0;
+
+static int octoIDNotYetSeen(octoMsgID id) {
+int steps = 0;
+	int endIndex = (searchStartIndex - 1) & (OCTO_ID_HISTORY_SIZE - 1);
+	for (int i = searchStartIndex; i != endIndex; i = ((i + 1) % OCTO_ID_HISTORY_SIZE)) {
+		if (octoIDHistory[i] == id) return false;
+		steps++;
 	}
+	return true;
+}
+
+static void addIDToOctoHistory(octoMsgID id) {
+	searchStartIndex = (searchStartIndex - 1) & (OCTO_ID_HISTORY_SIZE - 1);
+	octoIDHistory[searchStartIndex] = id;
 }
 
 class BLEScannerCallbacks : public BLEAdvertisedDeviceCallbacks {
 	void onResult(BLEAdvertisedDevice* advertisedDevice) {
 		if (advertisedDevice->haveServiceUUID()) {
 			// iOS
-			BLEUUID serviceUUID = advertisedDevice->getServiceUUID();
-			std::string UUID_string = serviceUUID.toString().c_str();
-			if (UUID_string == OCTO_UUID_iOS) {
+			BLEUUID uuid = advertisedDevice->getServiceUUID();
+			if (iOS_OCTO_UUID.equals(uuid)) {
 				std::string deviceName = advertisedDevice->getName();
-				if ((deviceName.length() == 16) &&
-					(deviceName != "0000000000000000") &&
-					(receivedOctoDatas.find(deviceName) == receivedOctoDatas.end())) {
-						recordOctoData(deviceName);
+				if (deviceName.length() == 16) {
+					octoMsgID id;
+					memcpy(&id, deviceName.c_str(), 8);
+					if ((id != allZeroMessageID) && octoIDNotYetSeen(id)) {
+						addIDToOctoHistory(id);
 						shape_id = deviceName.back() - '0';
-						if (shape_id < 0) shape_id = 255; // shouldn't happen
+						if (shape_id < 0) shape_id = 255; // ensure shape_id is positive
 						hasOctoMessage = true;
+					}
 				}
 			}
-		}
-
-		if (advertisedDevice->haveServiceData()) {
+		} else if (advertisedDevice->haveServiceData()) {
 			// Android
-			std::string serviceData = advertisedDevice->getServiceData();
-			BLEUUID serviceUUID = advertisedDevice->getServiceDataUUID();
-			std::string UUID_string = serviceUUID.toString().c_str();
-			if (UUID_string == OCTO_UUID_Android) {
-				if (receivedOctoDatas.find(serviceData) == receivedOctoDatas.end()) {
-					shape_id = serviceData[7];
-					recordOctoData(serviceData);
-					hasOctoMessage = true;
+			BLEUUID uuid = advertisedDevice->getServiceDataUUID();
+			if (ANDROID_OCTO_UUID.equals(uuid)) {
+				std::string serviceData = advertisedDevice->getServiceData();
+				if (serviceData.length() == 16) {
+					octoMsgID id;
+					memcpy(&id, serviceData.c_str(), 8);
+					if (octoIDNotYetSeen(id)) {
+						addIDToOctoHistory(id);
+						shape_id = serviceData[7];
+						hasOctoMessage = true;
+					}
 				}
 			}
 		}
@@ -218,17 +237,15 @@ static void scanComplete(BLEScanResults scanResults) {
 
 static void startBLEScanner() {
 	if (!bleScannerRunning) {
+		// initialize allZeroMessageID; ignore messages with that ID sent by iOS OctoStudio
+		memcpy(&allZeroMessageID, "00000000", 8);
+
 		pOctoScanner = BLEDevice::getScan();
 		pOctoScanner->setAdvertisedDeviceCallbacks(new BLEScannerCallbacks());
 		pOctoScanner->setMaxResults(0); // don't save results; use callback only
 		pOctoScanner->setActiveScan(true); // required by Octo
 		pOctoScanner->setDuplicateFilter(false); // good ???
-		// following setting do not seem to be necessary:
-		// pOctoScanner->setInterval(100);
-		// pOctoScanner->setWindow(100);
 		pOctoScanner->start(1, scanComplete, false);
-
-		receivedOctoDatas.clear();
 		bleScannerRunning = true;
 	}
 }
@@ -242,7 +259,7 @@ static OBJ primOctoStartBeam(int argCount, OBJ *args) {
 	BLE_pauseAdvertising();
 	pAdvertising = BLEDevice::getAdvertising();
 	pAdvertising->reset();
-	pAdvertising->addServiceUUID(OCTO_UUID_iOS);
+	pAdvertising->addServiceUUID(iOS_OCTO_UUID);
 	pAdvertising->setName(msg);
 	pAdvertising->setMinInterval(32);
 	pAdvertising->setMaxInterval(32);
@@ -253,7 +270,7 @@ static OBJ primOctoStartBeam(int argCount, OBJ *args) {
 static OBJ primOctoStopBeam(int argCount, OBJ *args) {
 	if (!pAdvertising) return falseObj; // not initialized thus not beaming
 
-	BLEDevice::getAdvertising()->removeServiceUUID(NimBLEUUID(OCTO_UUID_iOS));
+	BLEDevice::getAdvertising()->removeServiceUUID(iOS_OCTO_UUID);
 	BLE_resumeAdvertising();
 	return falseObj;
 }
@@ -284,7 +301,7 @@ static OBJ primScanReceive(int argCount, OBJ *args) {
 	return result;
 }
 
-#endif // new Octo primitives
+#endif // Octo primitives
 
 #if defined(BLE_KEYBOARD)
 
