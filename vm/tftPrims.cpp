3f7fc2998a8de1f7d6936a83a8cb9db5178163f1
Sean Shao <yue.shao@gmail.com>
2024-01-20 09:27:13 +0800

    添加掌控板oled 驱动到固件

--- vm/tftPrims.cpp
+++ vm/tftPrims.cpp
@@ -29,7 +29,8 @@ static int deferUpdates = false;
 	defined(ARDUINO_M5Stick_C) || defined(ARDUINO_ESP8266_WEMOS_D1MINI) || \
 	defined(ARDUINO_NRF52840_CLUE) || defined(ARDUINO_IOT_BUS) || defined(SCOUT_MAKES_AZUL) || \
 	defined(TTGO_RP2040) || defined(TTGO_DISPLAY) || defined(ARDUINO_M5STACK_Core2) || \
-	defined(GAMEPAD_DISPLAY) || defined(PICO_ED) || defined(OLED_128_64) || defined(FUTURE_LITE)
+	defined(GAMEPAD_DISPLAY) || defined(PICO_ED) || defined(OLED_128_64) || defined(FUTURE_LITE) || \
+	defined(TFT_TOUCH_SHIELD) || defined(OLED_1106)
 
 	#define BLACK 0
 	#define WHITE 65535
@@ -372,73 +373,7 @@ static int deferUpdates = false;
 			tftClear();
 			useTFT = true;
 		}
-
-		// M5 Core2 touchscreen support
-
-		#define HAS_TOUCH_SCREEN 1
-		#define CORE2_TOUCH_SCREEN_ADDR 0x38
-		#define CORE2_SCREEN_TOUCHED_PIN 39
-
-		static void setCore2TouchScreenReg(int regID, int value) {
-			Wire1.beginTransmission(CORE2_TOUCH_SCREEN_ADDR);
-			Wire1.write(regID);
-			Wire1.write(value);
-			Wire1.endTransmission();
-		}
-
-		static void touchInit() {
-			setCore2TouchScreenReg(0xA4, 0); // hold TOUCHED_PIN low while screen touched
-			pinMode(CORE2_SCREEN_TOUCHED_PIN, INPUT);
-			touchEnabled = true;
-		}
-
-		static uint32 lastTouchUpdate = 0;
-		static int touchScreenX = -1;
-		static int touchScreenY = -1;
-
-		static int screenTouched() {
-			if (!touchEnabled) touchInit();
-			return !digitalRead(CORE2_SCREEN_TOUCHED_PIN);
-		}
-
-		static void touchUpdate() {
-			if (!touchEnabled) touchInit();
-			uint32 now = millisecs();
-			if ((now - lastTouchUpdate) < 10) return;
-			if (screenTouched()) {
-				uint8 data[4];
-				Wire1.beginTransmission(CORE2_TOUCH_SCREEN_ADDR);
-				Wire1.write(3);
-				Wire1.endTransmission();
-				Wire1.requestFrom(CORE2_TOUCH_SCREEN_ADDR, sizeof(data));
-				for (int i = 0; i < sizeof(data); i++) {
-					data[i] = Wire1.read();
-				}
-				touchScreenX = ((data[0] & 0xF) << 8) | data[1];
-				touchScreenY = ((data[2] & 0xF) << 8) | data[3];
-			} else {
-				touchScreenX = -1;
-				touchScreenY = -1;
-			}
-			lastTouchUpdate = now;
-		}
-
-		static int screenTouchX() {
-			touchUpdate();
-			return touchScreenX;
-		}
-
-		static int screenTouchY() {
-			touchUpdate();
-			return touchScreenY;
-		}
-
-		static int screenTouchPressure() {
-			// pressure not supported; return a constant value if screen is touched, -1 if not
-			if (!touchEnabled) touchInit();
-			return screenTouched() ? 10 : -1;
-		}
-
+		
 	#elif defined(ARDUINO_NRF52840_CLUE)
 		#include "Adafruit_GFX.h"
 		#include "Adafruit_ST7789.h"
@@ -503,44 +438,25 @@ static int deferUpdates = false;
 			touchEnabled = true;
 		}
 
-		static int screenTouched() {
-			if (!touchEnabled) touchInit();
-			return ts.touched();
-		}
+	#elif defined(OLED_1106)
+		// #undef BLACK // defined in SSD1306 header
+		#include "Adafruit_GFX.h"
+		#include "Adafruit_SH110X.h"
 
-		static int screenTouchX() {
-			if (!touchEnabled) touchInit();
-			if (!ts.touched()) { return -1; }
-			uint16_t x, y;
-			uint8_t pressure;
-			ts.readData(&x, &y, &pressure);
-// 			x = (320 * (x - 256)) / 10;
-// 			if (x < 0) x = 0;
-// 			if (x > 320) x = 320;
-			return x;
-		}
+		#define TFT_WIDTH 128
+		#define TFT_HEIGHT 64
+		#define IS_MONOCHROME true
 
-		static int screenTouchY() {
-			if (!touchEnabled) touchInit();
-			if (!ts.touched()) { return -1; }
-			uint16_t x, y;
-			uint8_t pressure;
-			ts.readData(&x, &y, &pressure);
-// 			y = (240 * (y - 274)) / 14;
-// 			if (y < 0) y = 0;
-// 			if (y > 240) y = 240;
-			return y;
-		}
+		Adafruit_SH1106G tft = Adafruit_SH1106G(TFT_WIDTH, TFT_HEIGHT,&Wire, -1);
 
-		static int screenTouchPressure() {
-			if (!touchEnabled) touchInit();
-			if (!ts.touched()) { return -1; }
-			uint16_t x, y;
-			uint8_t pressure;
-			ts.readData(&x, &y, &pressure);
-			return pressure;
-		}
+		#undef UPDATE_DISPLAY
+		#define UPDATE_DISPLAY() { if (!deferUpdates) { tft.display(); taskSleep(10); }}
 
+		void tftInit() {
+			tft.begin(0x3C,true);
+			useTFT = true;
+			tftClear();
+		}
 	#elif defined(SCOUT_MAKES_AZUL)
 		#undef BLACK // defined in SSD1306 header
 		#include "Adafruit_GFX.h"
