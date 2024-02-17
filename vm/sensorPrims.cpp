185aeb7dd28e4e605dcc1889b5bcc9db6b2041dc
wwj718 <wuwenjie718@gmail.com>
2024-02-17 22:24:25 +0800

    将 vm/sensorPrims.cpp, 中文翻译 与官方最新版本合并

--- vm/sensorPrims.cpp
+++ vm/sensorPrims.cpp
@@ -2652,7 +2652,10 @@ static PrimEntry entries[] = {
 	{"spiSetup", primSPISetup},
 	{"readDHT", primReadDHT},
 	{"microphone", primMicrophone},
-
+	{"captureStart", captureStartPrim},
+	{"captureCount", primCaptureCount},
+	{"captureEnd", primCaptureEnd},
+	
 	#if defined(COCOROBO)
 	{"Humidity", primHumidity},		
   	#endif
