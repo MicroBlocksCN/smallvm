9520fb29529a2defed7cb09d91c7b3d5faf8f34a
ShaoYue <yue.shao@gmail.com>
2024-02-28 11:26:07 +0800

    添加明白板固件和温度传感器AHT20库

--- vm/outputPrims.cpp
+++ vm/outputPrims.cpp
@@ -460,7 +460,8 @@ static OBJ primLightLevel(int argCount, OBJ *args) {
 		lightLevel = lightLevel;
 	#elif defined(ARDUINO_CITILAB_ED1)
 		lightLevel = analogRead(34) * 1000 / 4095;
-	#elif defined(ARDUINO_Labplus_mPython) || defined(COCOROBO)
+	#elif defined(ARDUINO_Labplus_mPython) || defined(COCOROBO) || \
+		  defined(MINGBAI)
 		lightLevel = analogRead(39) * 1000 / 4095;
 	#elif defined(DATABOT)
 		const char *msg = "Use 'Light & Gesture' library on Databot.";
