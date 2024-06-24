c11b4e6da04c22373fb1ea735bd8789470fe3e2c
LiangShuai <sliang23@m.fudan.edu.cn>
2024-06-24 10:36:11 +0800

    增加CoCube显示Aruco标签的函数

--- vm/tftPrims.cpp
+++ vm/tftPrims.cpp
@@ -1380,128 +1380,47 @@ static OBJ primClear(int argCount, OBJ *args) {
 	return falseObj;
 }
 
-// Aruco and April tags
-
-const uint16_t aruco_tags[100] = {
-	0X4ACD, 0XF065, 0XCCD2, 0X66B9, 0XAB61, 0X8632, 0X61D1, 0X3B0D, 0X0125, 0X30A9, 0X066E, 0XEE58, 0XF148,
-	0XD5F0, 0XDB4E, 0XD9C1, 0XB99A, 0X99FF, 0X93A1, 0X8950, 0X7974, 0X4FD4, 0X332A, 0X227D, 0X01B8, 0X6B8E,
-	0X531B, 0X5AAB, 0XDEDC, 0XCB90, 0XBBEA, 0XA84D, 0X6130, 0X0F34, 0XF751, 0XF6D6, 0XE78A, 0XFB00, 0XF209,
-	0XE3A5, 0XE8E7, 0XD5D7, 0XCD73, 0XC74D, 0XDB17, 0XD114, 0XD2C0, 0XB49B, 0XAFD1, 0XAFEC, 0XAE6B, 0XAA97,
-	0XA2BE, 0XA068, 0X97FE, 0X9798, 0XEDB,  0X9E16, 0X94ED, 0X901A, 0X9820, 0X81E4, 0X7F5F, 0X7CBB, 0X745D,
-	0X6C85, 0X7B93, 0X7AD5, 0X7A63, 0X6376, 0X605E, 0X4483, 0X43FB, 0X49A4, 0X4037, 0X4854, 0X35E0, 0X369D,
-	0X26A7, 0X2C2A, 0X3367, 0X385F, 0X3AC8, 0X16A2, 0X06DA, 0X0444, 0X11D5, 0X08B2, 0XCA8A, 0X7552, 0X89E8,
-	0XF530, 0XF9B4, 0XD23E, 0XB627, 0XBC0B, 0XB0C9, 0XB02C, 0X961B, 0X8F38};
-
-const uint64_t april_tags[100] = {
-	0x0004064a19651ff1, 0x0004064a53f425b6, 0x0004064a8e832b7b, 0x0004064ac9123140, 0x0004064b03a13705,
-	0x0004064b3e303cca, 0x0004064b78bf428f, 0x0004064bb34e4854, 0x0004064beddd4e19, 0x0004064c286c53de,
-	0x0004064c62fb59a3, 0x0004064c9d8a5f68, 0x0004064d12a86af2, 0x0004064d4d3770b7, 0x0004064dc2557c41,
-	0x0004064dfce48206, 0x0004064e377387cb, 0x0004064e72028d90, 0x0004064eac919355, 0x0004064f21af9edf,
-	0x0004064fd15cb02e, 0x000406500bebb5f3, 0x00040650467abbb8, 0x00040650bb98c742, 0x00040650f627cd07,
-	0x000406516b45d891, 0x00040651a5d4de56, 0x000406521af2e9e0, 0x000406525581efa5, 0x00040653052f00f4,
-	0x000406533fbe06b9, 0x000406537a4d0c7e, 0x00040653ef6b1808, 0x0004065429fa1dcd, 0x0004065464892392,
-	0x000406549f182957, 0x00040654d9a72f1c, 0x00040655143634e1, 0x000406554ec53aa6, 0x000406558954406b,
-	0x00040655c3e34630, 0x00040655fe724bf5, 0x000406567390577f, 0x00040656ae1f5d44, 0x00040657233d68ce,
-	0x00040657985b7458, 0x00040657d2ea7a1d, 0x00040658480885a7, 0x00040658bd269131, 0x00040659e1f1ae0a,
-	0x0004065a919ebf59, 0x0004065bb669dc32, 0x0004065bf0f8e1f7, 0x0004065cdb34f90b, 0x0004065d15c3fed0,
-	0x0004065d50530495, 0x0004065e3a8f1ba9, 0x0004065eea3c2cf8, 0x0004066049964f96, 0x000406608425555b,
-	0x00040660beb45b20, 0x0004066133d266aa, 0x00040661e37f77f9, 0x000406621e0e7dbe, 0x00040662932c8948,
-	0x00040662cdbb8f0d, 0x00040663084a94d2, 0x0004066342d99a97, 0x000406637d68a05c, 0x00040663f286abe6,
-	0x0004066467a4b770, 0x00040664a233bd35, 0x00040664dcc2c2fa, 0x000406651751c8bf, 0x0004066551e0ce84,
-	0x00040666b13af122, 0x00040666ebc9f6e7, 0x0004066760e80271, 0x00040668109513c0, 0x000406684b241985,
-	0x00040668fad12ad4, 0x000406696fef365e, 0x00040669aa7e3c23, 0x00040669e50d41e8, 0x0004066bb9857010,
-	0x0004066bf41475d5, 0x0004066c6932815f, 0x0004066ca3c18724, 0x0004066d536e9873, 0x0004066dc88ca3fd,
-	0x0004066e031ba9c2, 0x0004066eb2c8bb11, 0x000406704cb1e374, 0x00040670c1cfeefe, 0x00040670fc5ef4c3,
-	0x0004067136edfa88, 0x00040671ac0c0612, 0x00040673bb1339ff, 0x000406746ac04b4e, 0x00040676b4568500};
-
-const int april_bit_x[52] = {
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 5, 4, 9, 9, 9, 9, 9, 9, 9, 9, 9, 6, 6, 6, 5,
-	9, 8, 7, 6, 5, 4, 3, 2, 1, 6, 5, 4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 4};
-const int april_bit_y[52] = {
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 4, 0, 1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 5, 4,
-	9, 9, 9, 9, 9, 9, 9, 9, 9, 6, 6, 6, 5, 9, 8, 7, 6, 5, 4, 3, 2, 1, 6, 5, 4, 5};
-
+#if defined(COCUBE)
 static OBJ primAruco(int argCount, OBJ *args) {
 	if (!useTFT) return falseObj;
 
-	int aruco_id = evalInt(args[0]);
-	if (aruco_id >= 100) {
-		return falseObj;
-	}
-	tft.drawRect(0, 0, TFT_HEIGHT, TFT_HEIGHT, BLACK);
-	const int cellSize = TFT_HEIGHT/8;
-	const int startX = TFT_WIDTH/2 - (4 * cellSize);
-	uint16_t tag = aruco_tags[aruco_id];
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			bool isBlack = false;
+	int aruco_id = obj2int(args[0]);
+	    if (aruco_id >= 100) {
+        return falseObj;
+    }
+    tft.drawRect(0, 0, 240, 240, WHITE);
+    const int cellSize = 30;
+    uint16_t tag = aruco_tags[aruco_id];
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            bool isBlack = false;
 
-			if (i == 0 || i == 7 || j == 0 || j == 7) {
-				// 外层白色边框 (outer white border)
-				isBlack = false;
-			} else if (i == 1 || i == 6 || j == 1 || j == 6) {
-				// 内层黑色边框 (inner black border)
-				isBlack = true;
-			} else {
-				// 中央的4x4区域，用于编码信息 (central 4x4 area for encoding information)
-				int bitIndex = (i - 2) * 4 + (j - 2);
-				isBlack = tag & (1 << (15 - bitIndex));
-			}
-			if (isBlack) {
-				tft.fillRect(startX + j * cellSize, i * cellSize, cellSize, cellSize, BLACK);
-			} else {
-				tft.fillRect(startX + j * cellSize, i * cellSize, cellSize, cellSize, WHITE);
-			}
-		}
-	}
-	tft.setCursor(startX + 2, 2);
-	tft.setTextColor(BLACK);
-	tft.setTextSize(2);
-	tft.print(aruco_id);
+            if (i == 0 || i == 7 || j == 0 || j == 7) {
+                // 外层白色边框
+                isBlack = false;
+            } else if (i == 1 || i == 6 || j == 1 || j == 6) {
+                // 内层黑色边框
+                isBlack = true;
+            } else {
+                // 中央的4x4区域，用于编码信息
+                int bitIndex = (i - 2) * 4 + (j - 2);
+                isBlack = tag & (1 << (15 - bitIndex));
+            }
+            if (isBlack) {
+                tft.fillRect(j * cellSize, i * cellSize, cellSize, cellSize, BLACK);
+            } else {
+                tft.fillRect(j * cellSize, i * cellSize, cellSize, cellSize, WHITE);
+            }
+        }
+    }
+    tft.setCursor(2, 2);
+    tft.setTextColor(BLACK);
+    tft.setTextSize(2);
+    tft.print(aruco_id);
 	UPDATE_DISPLAY();
 	return falseObj;
 }
-
-static OBJ primAprilTag(int argCount, OBJ *args) {
-	if (!useTFT) return falseObj;
-
-	int tag_id = evalInt(args[0]);
-	if (tag_id >= 100) {
-		return falseObj;
-	}
-	tft.drawRect(0, 0, TFT_HEIGHT, TFT_HEIGHT, BLACK);
-	const int cellSize = TFT_HEIGHT/10;
-	const int startX = TFT_WIDTH/2 - (5 * cellSize);
-	uint64_t codedata = april_tags[tag_id];
-
-	// 绘制外圈的黑色方块 (draw outer black square)
-	for (int i = 1; i < 9; i++) {
-		tft.fillRect(startX + i * cellSize, 1 * cellSize, cellSize, cellSize, BLACK); // 左边界 (left)
-		tft.fillRect(startX + i * cellSize, 8 * cellSize, cellSize, cellSize, BLACK); // 右边界 (right)
-		tft.fillRect(startX + 1 * cellSize, i * cellSize, cellSize, cellSize, BLACK); // 上边界 (top)
-		tft.fillRect(startX + 8 * cellSize, i * cellSize, cellSize, cellSize, BLACK); // 下边界 (bottom)
-	}
-
-	// 绘制内圈的白色方块 (draw inner ring of white squares)
-	for (int i = 2; i < 8; i++) {
-		tft.fillRect(startX + i * cellSize, 2 * cellSize, cellSize, cellSize, WHITE); // 左边界 (left)
-		tft.fillRect(startX + i * cellSize, 7 * cellSize, cellSize, cellSize, WHITE); // 右边界 (right)
-		tft.fillRect(startX + 2 * cellSize, i * cellSize, cellSize, cellSize, WHITE); // 上边界 (top)
-		tft.fillRect(startX + 7 * cellSize, i * cellSize, cellSize, cellSize, WHITE); // 下边界 (bottom)
-	}
-
-	// 绘制编码的标签图像 (draw encoded label)
-	for (int i = 0; i < 52; i++) {
-		int x = april_bit_x[i];
-		int y = april_bit_y[i];
-		bool bit = (codedata >> (51 - i)) & 1;
-		uint16_t color = bit ? WHITE : BLACK;
-		tft.fillRect(startX + x * cellSize, y * cellSize, cellSize, cellSize, color);
-	}
-	UPDATE_DISPLAY();
-	return falseObj;
-}
-
+#endif
 // display update control
 
 static OBJ primDeferUpdates(int argCount, OBJ *args) {
@@ -1790,7 +1709,7 @@ static PrimEntry entries[] = {
 	{"clear", primClear},
 	{"deferUpdates", primDeferUpdates},
 	{"resumeUpdates", primResumeUpdates},
-
+	
 	{"mergeBitmap", primMergeBitmap},
 	{"drawBuffer", primDrawBuffer},
 	{"drawBitmap", primDrawBitmap},
@@ -1801,6 +1720,10 @@ static PrimEntry entries[] = {
 	{"tftTouchPressure", primTftTouchPressure},
 
 	{"setVib",primSetVib},
+
+	#if defined(COCUBE)
+	{"aruco", primAruco},
+	#endif
 };
 
 void addTFTPrims() {
