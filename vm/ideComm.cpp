f1846cf64feb69833840c8e8ecdbf6498200fbc3
John Maloney <jhmaloney@gmail.com>
2024-06-27 06:28:25 -0400

    Checkpoint

--- vm/ideComm.cpp
+++ vm/ideComm.cpp
@@ -113,8 +113,9 @@ static uint16_t connID = -1;
 static uint32 lastSendTime = 0;
 static int lastRC = 0;
 
-#define RECV_BUF_MAX 1024
-static uint8_t bleRecvBuf[RECV_BUF_MAX];
+// incoming BLE buffer
+#define RECV_BUF_SIZE 1024
+static uint8_t bleRecvBuf[RECV_BUF_SIZE];
 static int bleBytesAvailable = 0;
 static int overRuns = 0;
 
@@ -152,7 +153,7 @@ static void updateConnectionState() {
 // BLE Operation
 
 static void bleReceiveData(const uint8_t *data, int byteCount) {
-	int available = RECV_BUF_MAX - bleBytesAvailable;
+	int available = RECV_BUF_SIZE - bleBytesAvailable;
 	if (byteCount > available) {
 		overRuns++;
 		byteCount = available;
@@ -293,62 +294,24 @@ void BLE_resumeAdvertising() {
 	if (serviceOnline) pAdvertising->start();
 }
 
-// IDE receive and send
-
-int recvBytes(uint8 *buf, int count) {
-	int bytesRead;
-
-	updateConnectionState();
-
-	if (!BLE_connected_to_IDE) { // no BLE connection; use Serial
-		bytesRead = Serial.available();
-		if (bytesRead > count) bytesRead = count; // there is only enough room for count bytes
-		return Serial.readBytes((char *) buf, bytesRead);
-	}
-
-	// use BLE connection
-	bytesRead = (count < bleBytesAvailable) ? count : bleBytesAvailable;
-	if (bytesRead == 0) return 0;
-
-	memcpy(buf, bleRecvBuf, bytesRead); // copy bytes to buf
-
-	int remainingBytes = bleBytesAvailable - bytesRead;
-	if (remainingBytes > 0) {
-		// remove bytesRead bytes from bleRecvBuf
-		memcpy(bleRecvBuf, &bleRecvBuf[bytesRead], remainingBytes);
-	}
-	bleBytesAvailable = remainingBytes;
-
-	return bytesRead;
-}
-
-int sendBytes(uint8 *buf, int start, int end) {
-	// Send bytes buf[start] through buf[end - 1] and return the number of bytes sent.
-
-	if (!BLE_connected_to_IDE) { // no BLE connection; use Serial
-		return Serial.write(&buf[start], end - start);
-	}
-
-	// use BLE connection
-	return bleSendData(&buf[start], end - start);
-}
-
-#elif PICO_BLUETOOTH
+#elif defined(PICO_BLUETOOTH)
 
 // uncomment these to test BLE
-// #include <BTstackLib.h>
-// #include <ble/att_server.h>
-// #include <ble/att_db_util.h>
+#include <BTstackLib.h>
+#include <ble/att_server.h>
 
 static int bleRunning = false;
+BLEDevice *bleDevice = NULL;
+
 static hci_con_handle_t connectionHandle = 0;
 static uint16_t txCharacteristic = 0;
+static uint32 lastSendTime = 0;
 
-#define BUF_SIZE 250 // 360 works, 380 fails; making both charactistics dynamic allows larger buffers
+#define BLE_BUF_MAX 250 // 360 works, 380 fails; making both charactistics dynamic allows larger buffers
 
 // incoming BLE buffer
-#define RECV_BUF_MAX 1024
-static uint8_t bleRecvBuf[RECV_BUF_MAX];
+#define RECV_BUF_SIZE 1024
+static uint8_t bleRecvBuf[RECV_BUF_SIZE];
 static int bleBytesAvailable = 0;
 static int overRuns = 0;
 
@@ -391,21 +354,33 @@ void setAdvertisingData(const char *name, UUID uuid) {
 // Pico connect/disconnect callbacks
 
 static void deviceConnectedCallback(BLEStatus status, BLEDevice *device) {
+	bleDevice = device;
 	if (BLE_STATUS_OK == status) {
 		connectionHandle = device->getHandle();
+		BTstack.stopAdvertising();
+		lastRcvTime = microsecs();
 		BLE_connected_to_IDE = true;
 	}
 }
 
 static void deviceDisconnectedCallback(BLEDevice *device) {
 	connectionHandle = 0;
+	BTstack.startAdvertising();
 	BLE_connected_to_IDE = false;
+}
+
+void bleDisconnect() {
+	if (connectionHandle) {
+		BLEDevice device(connectionHandle);
+		BTstack.bleDisconnect(&device);
+		connectionHandle = 0;
+	}
 }
 
 // Pico data receive callback
 
 static int gattWriteCallback(uint16_t attribute_handle, uint8_t *data, uint16_t byteCount) {
-	int available = RECV_BUF_MAX - bleBytesAvailable;
+	int available = RECV_BUF_SIZE - bleBytesAvailable;
 	if (byteCount > available) {
 		overRuns++;
 		byteCount = available;
@@ -415,10 +390,80 @@ static int gattWriteCallback(uint16_t attribute_handle, uint8_t *data, uint16_t 
 	return 0;
 }
 
+static void updateConnectionState() {
+	if ((BLE_connected_to_IDE || USB_connected_to_IDE) && !ideConnected()) {
+flashUserLED();
+		// we are no longer connected to the IDE
+		bleDisconnect(); // ensure BLE connection is closed
+		BTstack.startAdvertising();
+		BLE_connected_to_IDE = false;
+		USB_connected_to_IDE = false;
+	}
+	if (!USB_connected_to_IDE) { // either not connected or connected via BLE
+		if (Serial.available()) {
+			// new serial connection; disconnect BLE if it is connected
+			bleDisconnect();
+			BTstack.stopAdvertising();
+			BLE_connected_to_IDE = false;
+			// tell runtime that we've gotten a ping
+			lastRcvTime = microsecs();
+flashUserLED();
+			USB_connected_to_IDE = true;
+		}
+	}
+}
+
+static int bleSendData(uint8_t *data, int byteCount) {
+	if (byteCount <= 0) return 0;
+
+	// send byteCount bytes
+	if (byteCount > BLE_BUF_MAX) byteCount = BLE_BUF_MAX;
+// try: writeCharacteristicWithoutResponse
+	int err = att_server_notify(connectionHandle, txCharacteristic, data, byteCount);
+	return err ? 0 : byteCount;
+}
+
+void BLE_start() {
+	if (bleRunning) return; // BLE already running
+
+	// Initialize three letter ID and name
+	initBLEDeviceName("Pico");
+
+	// setup GATT database
+	BTstack.addGATTService(new UUID(MB_SERVICE_UUID));
+
+	txCharacteristic = BTstack.addGATTCharacteristicDynamic(new UUID(MB_CHARACTERISTIC_UUID_TX), ATT_PROPERTY_READ | ATT_PROPERTY_NOTIFY, BLE_BUF_MAX);
+	BTstack.addGATTCharacteristicDynamic(new UUID(MB_CHARACTERISTIC_UUID_RX), ATT_PROPERTY_WRITE | ATT_PROPERTY_WRITE_WITHOUT_RESPONSE, BLE_BUF_MAX);
+
+	// set callbacks
+	BTstack.setBLEDeviceConnectedCallback(deviceConnectedCallback);
+	BTstack.setBLEDeviceDisconnectedCallback(deviceDisconnectedCallback);
+	BTstack.setGATTCharacteristicWrite(gattWriteCallback);
+
+	// start BLE and advertising
+	BTstack.setup();
+	setAdvertisingData(bleDeviceName, UUID(MB_SERVICE_UUID)); // resume BLE advertisting
+	BTstack.startAdvertising();
+
+	bleRunning = true;
+}
+
+void BLE_stop() {
+	bleDisconnect();
+	BLE_connected_to_IDE = false;
+	bleRunning = false;
+}
+
+#endif
+
+#if defined(BLE_IDE) || defined(PICO_BLUETOOTH)
+
+// IDE receive and send (same for both version of BLE)
+
 int recvBytes(uint8 *buf, int count) {
 	int bytesRead;
 
-//	updateConnectionState(); // xxx todo for Pico
+	updateConnectionState();
 
 	if (!BLE_connected_to_IDE) { // no BLE connection; use Serial
 		bytesRead = Serial.available();
@@ -445,48 +490,12 @@ int recvBytes(uint8 *buf, int count) {
 int sendBytes(uint8 *buf, int start, int end) {
 	// Send bytes buf[start] through buf[end - 1] and return the number of bytes sent.
 
-// 	if (!BLE_connected_to_IDE || !connectionHandle) { // no BLE connection; use Serial
-// 		return Serial.write(&buf[start], end - start);
-// 	}
+	if (!BLE_connected_to_IDE) { // no BLE connection; use Serial
+		return Serial.write(&buf[start], end - start);
+	}
 
-	int byteCount = end - start;
-	if (byteCount > BUF_SIZE) byteCount = BUF_SIZE;
-	int status = att_server_notify(connectionHandle, txCharacteristic, &buf[start], byteCount);
-	return (status ? 0 : byteCount);
-}
-
-void att_db_util_add_client_characteristic_configuration(uint16_t flags);
-
-void BLE_start() {
-	if (bleRunning) return; // BLE already running
-
-	// Initialize three letter ID and name
-	initBLEDeviceName("Pico");
-
-	// setup GATT database
-	BTstack.addGATTService(new UUID(MB_SERVICE_UUID));
-
-	txCharacteristic = BTstack.addGATTCharacteristicDynamic(new UUID(MB_CHARACTERISTIC_UUID_TX), ATT_PROPERTY_READ | ATT_PROPERTY_NOTIFY, BUF_SIZE);
-//	BTstack.addGATTCharacteristicDynamic(new UUID(MB_CHARACTERISTIC_UUID_RX), ATT_PROPERTY_WRITE | ATT_PROPERTY_WRITE_WITHOUT_RESPONSE, BUF_SIZE);
-	BTstack.addGATTCharacteristicDynamic(new UUID(MB_CHARACTERISTIC_UUID_RX), ATT_PROPERTY_WRITE_WITHOUT_RESPONSE, BUF_SIZE);
-
-	// set callbacks
-	BTstack.setBLEDeviceConnectedCallback(deviceConnectedCallback);
-	BTstack.setBLEDeviceDisconnectedCallback(deviceDisconnectedCallback);
-	BTstack.setGATTCharacteristicWrite(gattWriteCallback);
-
-	// start BLE and advertising
-	BTstack.setup();
-	setAdvertisingData(bleDeviceName, UUID(MB_SERVICE_UUID));
-	BTstack.startAdvertising();
-
-	bleRunning = true;
-}
-
-void BLE_stop() {
-	// xxx to do
-	bleRunning = false;
-	BLE_connected_to_IDE = false;
+	// use BLE connection
+	return bleSendData(&buf[start], end - start);
 }
 
 #else
