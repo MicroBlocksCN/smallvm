44da3f339920d695411b58a1e393072e58d08ebf
LiangShuai <sliang23@m.fudan.edu.cn>
2024-07-23 01:22:33 +0800

    add motor encoders function

--- vm/sensorPrims.cpp
+++ vm/sensorPrims.cpp
@@ -2730,6 +2730,7 @@ static OBJ primMicrophone(int argCount, OBJ *args) {
 
 	void cocubeSensorUpdate(){
 		cocube.Update();
+		cocube.EncoderUpdate();
 	}
 
 	static OBJ primPositionX(int argCount, OBJ *args){
@@ -2744,6 +2745,16 @@ static OBJ primMicrophone(int argCount, OBJ *args) {
 
 	static OBJ primPositionYaw(int argCount, OBJ *args){
 			int result = cocube.GetAngle();
+			return int2obj(result);
+	}
+
+	static OBJ primPositionSpeedLeft(int argCount, OBJ *args){
+			int result = cocube.GetSpeedLeft();
+			return int2obj(result);
+	}
+
+	static OBJ primPositionSpeedRight(int argCount, OBJ *args){
+			int result = cocube.GetSpeedRight();
 			return int2obj(result);
 	}
 #endif
@@ -2841,6 +2852,8 @@ static PrimEntry entries[] = {
 	{"position_x", primPositionX},
 	{"position_y", primPositionY},
 	{"position_yaw", primPositionYaw},
+	{"speed_left", primPositionSpeedLeft},
+	{"speed_right", primPositionSpeedRight},
   	#endif
 };
 
