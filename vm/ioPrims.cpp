e8de6fe65abf162b237a9f07f9a4b053ad9d8f25
LiangShuai <sliang23@m.fudan.edu.cn>
2024-08-13 12:58:03 +0800

    增加CoCube设置用户LED的功能

--- vm/ioPrims.cpp
+++ vm/ioPrims.cpp
@@ -2008,7 +2008,7 @@ void primSetUserLED(OBJ *args) {
 		}
 	#elif defined(ARDUINO_CITILAB_ED1) || defined(ARDUINO_M5Stack_Core_ESP32) || defined(ARDUINO_M5STACK_FIRE) || \
 		defined(ARDUINO_M5STACK_Core2) || defined(TTGO_DISPLAY) || defined(M5_CARDPUTER) || \
-		defined(FUTURE_LITE)
+		defined(FUTURE_LITE) || defined(COCUBE)
 			tftSetHugePixel(3, 1, (trueObj == args[0]));
 	#else
 		if (useTFT) {
