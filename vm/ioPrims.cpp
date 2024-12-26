c5d62a7a68f7262848a92ec03c7ed54e415a12bb
ShaoYue <yue.shao@gmail.com>
2024-12-26 17:01:28 +0800

    add 8x8 点阵库

--- vm/ioPrims.cpp
+++ vm/ioPrims.cpp
@@ -656,10 +656,10 @@ void hardwareInit() {
 	#define PIN_LED 10
 	#define INVERT_USER_LED true
 	static const char reservedPin[TOTAL_PINS] = {
-		0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
-		0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-		1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
-		1, 1, 0, 0, 0, 1, 0, 0, 1, 0};
+		0, 1, 0, 1, 0, 0, 1, 1, 1, 1,
+		0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 1, 0, 0, 0, 1, 1,
+		1, 1, 0, 0, 0, 0, 0, 0, 0, 0};
 
 #elif defined(M5Atom_Matrix)
 
