c2620ac23a1509230bb86bdd2695037c1a370146
Crafter-feng <weiyanfengv@gmail.com>
2025-11-29 21:17:01 +0800

    - Added initialization of the SERIAL_PORT with custom RX/TX pins (13, 14) for ARDUINO_Labplus_mPython.

--- vm/serialPrims.cpp
+++ vm/serialPrims.cpp
@@ -255,14 +255,9 @@ static void serialOpen(int baudRate) {
 		SERIAL_PORT.begin(baudRate);
 		delayMicroseconds(5); // wait for garbage byte when first opening the serial port after a reset (seen at 115200 baud)
 		SERIAL_PORT.begin(baudRate); // reset to discard garbage byte
-	#elif defined(ESP32_C3)
-		#if !defined(ARDUINO_USB_MODE)
-			SERIAL_PORT.begin(baudRate, SERIAL_8N1, 18, 19);
-		#else
-			SERIAL_PORT.begin(baudRate, SERIAL_8N1, RX, TX);
-		#endif
-	#elif defined(ESP32)
-		// all ESP32 boards that do not have cases above
+	#elif defined(ARDUINO_Labplus_mPython)
+		SERIAL_PORT.begin(baudRate, SERIAL_8N1, 13, 14);
+	#elif defined(ESP32_ORIGINAL)
 		SERIAL_PORT.begin(baudRate, SERIAL_8N1, 16, 17);
 	#elif defined(DUELink)
 		if (DUE_HAS_EDGE_CONNECTOR) {
